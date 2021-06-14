9827421b3d3f9a7e25f49515f178e190ea033973
Eric Scouten <eric@scouten.com>
2021-06-13 21:00:16 -0700

    Day 3: To Telluride.

---
+++ content/2021/04-21+dallas-to-seattle/day-3-to-telluride/markers.js
@@ -0,0 +1,22 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4192-106", 36.406517, -105.571700),
+        addGpxMarker(map, "es-4192-109", 36.996011, -105.997331),
+        addGpxMarker(map, "es-4192-113", 37.284272, -106.038033),
+        addGpxMarker(map, "es-4193-001", 37.863609, -106.146319),
+        addGpxMarker(map, "es-4193-007", 37.893667, -106.154225),
+        addGpxMarker(map, "es-4192-119", 37.893394, -106.147667),
+        addGpxMarker(map, "es-4192-145p", 38.269642, -106.725808),
+        addGpxMarker(map, "es-4192-174p", 38.285258, -106.747397),
+        addGpxMarker(map, "es-4192-203", 38.450333, -106.757186),
+        addGpxMarker(map, "es-4192-208", 38.151481, -107.749611),
+        addGpxMarker(map, "es-4192-209", 38.126736, -107.831581),
+        addGpxMarker(map, "es-4192-230", 38.099861, -107.912506),
+        addGpxMarker(map, "es-4192-239", 37.937756, -107.813003),
+        addGpxMarker(map, "es-4195-002", 37.946397, -107.833825),
+        addGpxMarker(map, "es-4195-003", 37.946281, -107.833192),
+        addGpxMarker(map, "es-4195-004", 37.946097, -107.832414),
+        addGpxMarker(map, "es-4195-005", 37.945900, -107.831681),
+        addGpxMarker(map, "es-4195-006", 37.945675, -107.830872),
+    ]
+}
