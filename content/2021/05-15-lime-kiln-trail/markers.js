b02992d450d50b4762f8968c0584e63b0d1ff28f
Eric Scouten <eric@scouten.com>
2021-06-27 21:00:28 -0700

    Lime Kiln Trail.

---
+++ content/2021/05-15-lime-kiln-trail/markers.js
@@ -0,0 +1,18 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4203-004", 48.077442, -121.932311),
+        addGpxMarker(map, "es-4203-006", 48.095775, -121.927842),
+        addGpxMarker(map, "es-4203-010", 48.096256, -121.927253),
+        addGpxMarker(map, "es-4203-014", 48.096436, -121.926889),
+        addGpxMarker(map, "es-4203-017", 48.096469, -121.926833),
+        addGpxMarker(map, "es-4203-018", 48.096939, -121.926369),
+        addGpxMarker(map, "es-4203-021", 48.101181, -121.918481),
+        addGpxMarker(map, "es-4203-022", 48.102858, -121.915931),
+        addGpxMarker(map, "es-4203-025", 48.102833, -121.915869),
+        addGpxMarker(map, "es-4203-028", 48.102761, -121.915711),
+        addGpxMarker(map, "es-4203-029", 48.102872, -121.915947),
+        addGpxMarker(map, "es-4203-035", 48.102961, -121.915908),
+        addGpxMarker(map, "es-4203-039", 48.106981, -121.907669),
+        addGpxMarker(map, "es-4203-049", 48.098197, -121.925117),
+    ]
+}
