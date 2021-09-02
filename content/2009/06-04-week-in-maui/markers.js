a6207569b66aefeb3fc40c449d484931dc0309d6
Eric Scouten <eric@scouten.com>
2021-09-01 19:45:28 -0700

    A Week in Mau’i.

---
+++ content/2009/06-04-week-in-maui/markers.js
@@ -0,0 +1,22 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-1437-001", 21.022095, -156.627212),
+        addGpxMarker(map, "es-1437-022", 21.022395, -156.620922),
+        addGpxMarker(map, "es-1437-030", 21.000237, -156.553378),
+        addGpxMarker(map, "es-1437-050", 20.996868, -156.546755),
+        addGpxMarker(map, "es-1437-052", 20.991538, -156.545010),
+        addGpxMarker(map, "es-1438-009", 20.938928, -156.692823),
+        addGpxMarker(map, "es-1438-025", 20.938658, -156.692795),
+        addGpxMarker(map, "es-1440-011a", 20.864810, -156.167340),
+        addGpxMarker(map, "es-1440-172", 20.838420, -156.139350),
+        addGpxMarker(map, "es-1443-382", 20.940320, -156.692610),
+        addGpxMarker(map, "es-1445-020", 20.768740, -156.458950),
+        addGpxMarker(map, "es-1445-053", 20.766550, -156.458770),
+        addGpxMarker(map, "es-1445-068", 20.765950, -156.458890),
+        addGpxMarker(map, "es-1445-095", 20.765950, -156.458890),
+        addGpxMarker(map, "es-1447-026", 20.938840, -156.692880),
+        addGpxMarker(map, "es-1447-094", 20.938300, -156.692830),
+        addGpxMarker(map, "es-1448-113", 20.871360, -156.677730),
+        addGpxMarker(map, "es-1448-212a", 20.873900, -156.677650),
+    ]
+}
