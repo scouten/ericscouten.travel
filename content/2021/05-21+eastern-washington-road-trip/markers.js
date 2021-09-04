a788927b173ea6767ae4499019ca9a6f7c877c46
Eric Scouten <eric@scouten.com>
2021-09-04 00:07:05 -0700

    Cross-link to 146parks for the parks on this road trip.

--- content/2021/05-21+eastern-washington-road-trip/markers.js
+++ content/2021/05-21+eastern-washington-road-trip/markers.js
@@ -9,5 +9,14 @@ function addGpxMarkers(map) {
         addGpxMarker(map, "es-4206-113", 47.129578, -117.242469),
         addGpxMarker(map, "es-4207-011", 46.824994, -117.526175),
         addGpxMarker(map, "es-4213-616p", 46.996953, -120.548425),
+
+        addParkMarker(map, 33, "Steptoe Butte", "steptoe-butte", 47.0321, -117.2967, "es-4206-029p"),
+        addParkMarker(map, 34, "Steptoe Battlefield", "steptoe-battlefield", 47.2285, -117.3649, "es-4206-139"),
+        addParkMarker(map, 35, "Lyons Ferry", "lyons-ferry", 46.5953, -118.2185, "es-4206-304"),
+        addParkMarker(map, 36, "Sacajawea", "sacajawea", 46.1985, -119.0396, "es-4206-352"),
+        addParkMarker(map, 37, "Brooks Memorial", "brooks-memorial", 45.9498, -120.6655, "es-4213-159"),
+        addParkMarker(map, 38, "Fort Simcoe", "fort-simcoe", 46.3425, -120.8391, "es-4213-261p"),
+        addParkMarker(map, 39, "Yakima Sportsman", "yakima-sportsman", 46.5925, -120.4602, "es-4213-381p"),
+        addParkMarker(map, 40, "Olmstead Place", "olmstead-place", 46.975, -120.469, "es-4213-576p"),
     ]
 }
