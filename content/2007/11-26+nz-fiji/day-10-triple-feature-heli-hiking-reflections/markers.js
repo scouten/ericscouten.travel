09352ba59040b4e5ce12c0a01bea7fe8f8a8501c
Eric Scouten <eric@scouten.com>
2021-10-10 18:56:28 -0700

    Day 10: Triple Feature: Heli-Hiking and Reflections.

---
+++ content/2007/11-26+nz-fiji/day-10-triple-feature-heli-hiking-reflections/markers.js
@@ -0,0 +1,7 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-1240-034", -43.460848, 170.188893),
+        addGpxMarker(map, "es-1241-005", -43.461827, 170.186998),
+        addGpxMarker(map, "es-1242-031", -43.437812, 169.963103),
+    ]
+}
