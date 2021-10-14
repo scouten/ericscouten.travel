4faf1cb46529573ba3b582449b64d3ce360ed12c
Eric Scouten <eric@scouten.com>
2021-10-14 09:58:57 -0700

    Day 4: Amsterdam.

---
+++ content/2008/04-13+netherlands/day-4-amsterdam/markers.js
@@ -0,0 +1,10 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-1291-008", 52.366815, 4.889893),
+        addGpxMarker(map, "es-1291-052", 52.366630, 4.890277),
+        addGpxMarker(map, "es-1291-081", 52.375318, 4.883993),
+        addGpxMarker(map, "es-1292-009", 52.375665, 4.884000),
+        addGpxMarker(map, "es-1292-011", 52.376075, 4.883788),
+        addGpxMarker(map, "es-1292-026", 52.375258, 4.883755),
+    ]
+}
