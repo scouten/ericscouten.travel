9df8d4d411ce27485bc595c49d7e10b442c584f9
Eric Scouten <eric@scouten.com>
2021-10-14 21:13:43 -0700

    Day 5: Haarlem and Katwijk.

---
+++ content/2008/04-13+netherlands/day-5-haarlem-katwijk/markers.js
@@ -0,0 +1,13 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-1293-033", 52.201495, 4.393040),
+        addGpxMarker(map, "es-1293-073", 52.202178, 4.392698),
+        addGpxMarker(map, "es-1293-079", 52.202060, 4.392676),
+        addGpxMarker(map, "es-1294-076", 52.379248, 4.633578),
+        addGpxMarker(map, "es-1294-080", 52.377713, 4.631768),
+        addGpxMarker(map, "es-1294-081", 52.377670, 4.631718),
+        addGpxMarker(map, "es-1294-084", 52.376927, 4.631093),
+        addGpxMarker(map, "es-1294-089", 52.376810, 4.631408),
+        addGpxMarker(map, "es-1294-099", 52.473920, 4.818077),
+    ]
+}
