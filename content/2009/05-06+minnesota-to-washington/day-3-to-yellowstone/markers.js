e90ddc6b8ce47e1833316eea4b7879234df60442
Eric Scouten <eric@scouten.com>
2021-10-22 20:56:32 -0700

    Day 3: To Yellowstone.

---
+++ content/2009/05-06+minnesota-to-washington/day-3-to-yellowstone/markers.js
@@ -0,0 +1,12 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-1426-003", 45.112782, -109.301750),
+        addGpxMarker(map, "es-1426-007", 45.055217, -109.407987),
+        addGpxMarker(map, "es-1427-001", 44.510053, -109.153875),
+        addGpxMarker(map, "es-1427-020", 44.506425, -110.276230),
+        addGpxMarker(map, "es-1427-037", 44.567023, -110.382965),
+        addGpxMarker(map, "es-1427-075", 44.624523, -110.433992),
+        addGpxMarker(map, "es-1427-160", 44.734610, -110.519560),
+        addGpxMarker(map, "es-1427-267", 44.649222, -110.946783),
+    ]
+}
