a2c6ae75f8d8ee5c1e700b86512f5453481d04ff
Eric Scouten <eric@scouten.com>
2021-11-05 19:46:48 -0700

    Nisqually NWR.

---
+++ content/2021/09-06-nisqually-nwr/markers.js
@@ -0,0 +1,9 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4253-006", 47.078064, -122.711722),
+        addGpxMarker(map, "es-4253-008", 47.082814, -122.721397),
+        addGpxMarker(map, "es-4253-009", 47.093242, -122.724394),
+        addGpxMarker(map, "es-4253-010", 47.093147, -122.724533),
+        addGpxMarker(map, "es-4253-012", 47.092200, -122.725869),
+    ]
+}
