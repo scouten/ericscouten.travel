76f86f62dbbd1d8325bd061b1e962ed88469f9a4
Eric Scouten <eric@scouten.com>
2021-11-21 08:33:30 -0800

    Bellingham Sunrise.

---
+++ content/2021/11-21-bellingham-sunrise/markers.js
@@ -0,0 +1,7 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4274-007", 48.753597, -122.496200),
+        addGpxMarker(map, "es-4274-016", 48.753872, -122.495833),
+        addGpxMarker(map, "es-4274-018p", 48.753433, -122.496544),
+    ]
+}
