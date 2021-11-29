0128c5516fbd0ce2ff3d29e7442f99a703d048d0
Eric Scouten <eric@scouten.com>
2021-11-28 23:42:04 -0800

    Day 1: To Omak.

---
+++ content/2009/12-27+winter-in-eastern-washington/day-1-to-omak/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-1563-010", 47.656588, -120.126100),
+        addGpxMarker(map, "es-1564-004", 47.728888, -120.044250),
+        addGpxMarker(map, "es-1564-005", 47.728888, -120.044252),
+        addGpxMarker(map, "es-1564-029", 47.728887, -120.044277),
+        addGpxMarker(map, "es-1564-036", 47.699817, -119.968773),
+        addGpxMarker(map, "es-1564-051", 47.699802, -119.942722),
+        addGpxMarker(map, "es-1564-076", 47.612665, -119.814238),
+        addGpxMarker(map, "es-1564-087", 47.612645, -119.953183),
+        addGpxMarker(map, "es-1564-101", 47.815718, -119.710375),
+        addGpxMarker(map, "es-1564-104", 47.844565, -119.706568),
+        addGpxMarker(map, "es-1564-117", 47.834598, -119.706460),
+    ]
+}
