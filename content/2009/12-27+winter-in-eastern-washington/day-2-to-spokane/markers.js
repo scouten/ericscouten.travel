9ec53f0651c4d25b0e939e220803aa6bcdc05ffa
Eric Scouten <eric@scouten.com>
2021-11-29 19:54:45 -0800

    Day 2: To Spokane.

---
+++ content/2009/12-27+winter-in-eastern-washington/day-2-to-spokane/markers.js
@@ -0,0 +1,22 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-1565-003", 48.389375, -119.885283),
+        addGpxMarker(map, "es-1565-015", 48.361123, -120.034248),
+        addGpxMarker(map, "es-1565-016", 48.344557, -120.058525),
+        addGpxMarker(map, "es-1565-020", 48.346520, -120.065713),
+        addGpxMarker(map, "es-1565-026", 48.346518, -120.065705),
+        addGpxMarker(map, "es-1565-031", 47.986707, -119.648517),
+        addGpxMarker(map, "es-1565-043", 47.949883, -119.510708),
+        addGpxMarker(map, "es-1565-051", 47.948958, -119.507370),
+        addGpxMarker(map, "es-1565-056", 47.918993, -119.394018),
+        addGpxMarker(map, "es-1565-058", 48.003220, -118.895880),
+        addGpxMarker(map, "es-1565-064", 47.994115, -118.874593),
+        addGpxMarker(map, "es-1565-087", 47.947102, -118.690843),
+        addGpxMarker(map, "es-1565-088", 47.947137, -118.690910),
+        addGpxMarker(map, "es-1565-094", 47.947140, -118.690918),
+        addGpxMarker(map, "es-1565-103", 47.930303, -118.690978),
+        addGpxMarker(map, "es-1565-109", 47.865277, -118.726348),
+        addGpxMarker(map, "es-1565-122", 47.862252, -118.724128),
+        addGpxMarker(map, "es-1565-124", 47.862252, -118.724128),
+    ]
+}
