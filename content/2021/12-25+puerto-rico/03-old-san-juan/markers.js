fb1c21eb65a0d91de7b567498ff343449f26b6fc
Eric Scouten <eric@scouten.com>
2022-01-04 19:05:32 -0800

    Day 3: Old San Juan.

---
+++ content/2021/12-25+puerto-rico/03-old-san-juan/markers.js
@@ -0,0 +1,16 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4290-003", 18.466089, -66.114267),
+        addGpxMarker(map, "es-4290-011", 18.465153, -66.118119),
+        addGpxMarker(map, "es-4290-014", 18.465069, -66.118606),
+        addGpxMarker(map, "es-4291-005", 18.465049, -66.118675),
+        addGpxMarker(map, "lr-1182", 18.464697, -66.119689),
+        addGpxMarker(map, "lr-1183", 18.466050, -66.120758),
+        addGpxMarker(map, "lr-1186", 18.466022, -66.121544),
+        addGpxMarker(map, "es-4290-031", 18.466258, -66.122331),
+        addGpxMarker(map, "es-4290-039", 18.466269, -66.122336),
+        addGpxMarker(map, "es-4290-048", 18.465314, -66.119392),
+        addGpxMarker(map, "es-4290-058", 18.465200, -66.116700),
+        addGpxMarker(map, "es-4290-061", 18.465297, -66.116639),
+    ]
+}
