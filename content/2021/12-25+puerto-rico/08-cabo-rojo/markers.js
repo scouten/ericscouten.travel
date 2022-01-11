13310c14ab4d04b462d32489e2e2de82da32debe
Eric Scouten <eric@scouten.com>
2022-01-10 19:57:29 -0800

    Day 8: Cabo Rojo.

---
+++ content/2021/12-25+puerto-rico/08-cabo-rojo/markers.js
@@ -0,0 +1,21 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4299-008p", 17.934003, -67.193825),
+        addGpxMarker(map, "es-4299-025", 17.934008, -67.193825),
+        addGpxMarker(map, "es-4299-027", 17.933783, -67.192467),
+        addGpxMarker(map, "lr-1383", 17.933381, -67.192008),
+        addGpxMarker(map, "es-4299-035", 17.933611, -67.192025),
+        addGpxMarker(map, "es-4299-046", 17.934636, -67.191567),
+        addGpxMarker(map, "es-4299-055", 17.935214, -67.190636),
+        addGpxMarker(map, "es-4299-057", 17.935867, -67.191789),
+        addGpxMarker(map, "es-4299-058", 17.936050, -67.192514),
+        addGpxMarker(map, "es-4299-063", 17.942597, -67.191467),
+        addGpxMarker(map, "es-4299-065", 18.147401, -67.094634),
+        addGpxMarker(map, "es-4299-066", 18.147401, -67.094634),
+        addGpxMarker(map, "es-4299-067", 18.147401, -67.094634),
+        addGpxMarker(map, "lr-1401", 18.147401, -67.094634),
+        addGpxMarker(map, "es-4299-079", 18.147401, -67.094634),
+        addGpxMarker(map, "es-4299-081", 18.147401, -67.094634),
+        addGpxMarker(map, "es-4301-014", 18.147401, -67.094634),
+    ]
+}
