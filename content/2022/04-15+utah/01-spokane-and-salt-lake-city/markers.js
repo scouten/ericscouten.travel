a9b0e5eb54d461e1ca09dfa60e4f9a85a9b90d35
Eric Scouten <eric@scouten.com>
2022-05-02 13:56:34 -0700

    Day 1: Spokane and Salt Lake City.

---
+++ content/2022/04-15+utah/01-spokane-and-salt-lake-city/markers.js
@@ -0,0 +1,8 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4329-007", 45.658640, -116.096987),
+        addGpxMarker(map, "es-4329-013", 43.528416, -114.399938),
+        addGpxMarker(map, "es-4329-014", 41.430315, -112.406394),
+        addGpxMarker(map, "es-4329-019", 40.723526, -112.088612),
+    ]
+}
