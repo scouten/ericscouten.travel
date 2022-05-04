88ddad5730051b632cd0aa1ebbdc6a14de7d503d
Eric Scouten <eric@scouten.com>
2022-05-04 12:30:11 -0700

    Day 3: Saltair and Antelope Island.

---
+++ content/2022/04-15+utah/03-saltair-and-antelope-island/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4331-008", 40.748736, -112.187425),
+        addGpxMarker(map, "es-4331-018", 40.750972, -112.197967),
+        addGpxMarker(map, "es-4331-044", 40.750978, -112.197897),
+        addGpxMarker(map, "es-4331-057", 41.069367, -112.227486),
+        addGpxMarker(map, "es-4331-068", 41.069369, -112.227478),
+        addGpxMarker(map, "es-4331-074", 41.069333, -112.227592),
+        addGpxMarker(map, "es-4331-086", 40.861711, -112.172697),
+    ]
+}
