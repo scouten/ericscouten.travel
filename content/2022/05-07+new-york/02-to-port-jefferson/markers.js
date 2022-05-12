fb7a593ae93ada4328ef7b679568a8297ae5f31a
Eric Scouten <eric@scouten.com>
2022-05-11 23:46:20 -0400

    Day 2: To Port Jefferson.

---
+++ content/2022/05-07+new-york/02-to-port-jefferson/markers.js
@@ -0,0 +1,13 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4341-018p", 41.235631, -72.985694),
+        addGpxMarker(map, "es-4341-045", 41.173225, -73.182289),
+        addGpxMarker(map, "es-4342-049", 40.961033, -73.085342),
+        addGpxMarker(map, "es-4342-051", 40.960208, -73.084992),
+        addGpxMarker(map, "es-4342-054", 40.955600, -73.079400),
+        addGpxMarker(map, "es-4342-062", 40.951236, -73.073892),
+        addGpxMarker(map, "es-4342-070", 40.948169, -73.070472),
+        addGpxMarker(map, "es-4342-079", 40.948444, -73.067925),
+        addGpxMarker(map, "es-4342-085", 40.948447, -73.068000),
+    ]
+}
