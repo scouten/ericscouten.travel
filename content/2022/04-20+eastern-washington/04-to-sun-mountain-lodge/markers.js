a74b30aebf2bf815f3adeca334b36f1aa0a8e265
Eric Scouten <eric@scouten.com>
2022-05-14 20:25:49 -0700

    Day 4: To Sun Mountain Lodge.

---
+++ content/2022/04-20+eastern-washington/04-to-sun-mountain-lodge/markers.js
@@ -0,0 +1,10 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4334-045", 47.486503, -119.319847),
+        addParkMarker(map, 97, "Conconully", "conconully", 48.556, -119.752, "es-4334-112"),
+        addGpxMarker(map, "es-4334-132", 48.478053, -120.256989),
+        addGpxMarker(map, "es-4334-140", 48.477647, -120.256753),
+        addGpxMarker(map, "es-4334-148", 48.477864, -120.256950),
+        addGpxMarker(map, "es-4334-151", 48.477628, -120.257400),
+    ]
+}
