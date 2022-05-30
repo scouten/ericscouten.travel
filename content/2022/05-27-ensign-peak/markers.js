1206cd34f232f9cca521c814bf959ed9535e821c
Eric Scouten <eric@scouten.com>
2022-05-29 22:10:49 -0700

    Ensign Peak, Salt Lake City.

---
+++ content/2022/05-27-ensign-peak/markers.js
@@ -0,0 +1,12 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4358-006", 40.794197, -111.888564),
+        addGpxMarker(map, "es-4358-007", 40.794644, -111.888772),
+        addGpxMarker(map, "es-4358-010", 40.795356, -111.890289),
+        addGpxMarker(map, "es-4358-015", 40.794653, -111.891031),
+        addGpxMarker(map, "es-4358-017p", 40.794289, -111.890564),
+        addGpxMarker(map, "es-4358-043", 40.794481, -111.890961),
+        addGpxMarker(map, "es-4358-054", 40.794947, -111.891053),
+        addGpxMarker(map, "es-4358-057", 40.794406, -111.888794),
+    ]
+}
