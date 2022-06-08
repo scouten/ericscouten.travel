19c3b4a771c57130c5bc88aae8fb0013c3465fcf
Eric Scouten <eric@scouten.com>
2022-06-07 21:53:44 -0700

    Day 6: Stansbury Island

---
+++ content/2022/05-27+utah/06-stansbury-island/markers.js
@@ -0,0 +1,8 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "lr-1988", 40.749834, -112.521421),
+        addGpxMarker(map, "es-4361-088", 40.749825, -112.521414),
+        addGpxMarker(map, "es-4361-140", 40.913483, -112.527886),
+        addGpxMarker(map, "es-4361-148", 40.760475, -112.518525),
+    ]
+}
