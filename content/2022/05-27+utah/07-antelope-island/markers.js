2926c527ecf5c4a7239a6f4674ba5dcd29cb7b0b
Eric Scouten <eric@scouten.com>
2022-06-19 13:07:52 -0700

    Day 7: Antelope Island State Park

---
+++ content/2022/05-27+utah/07-antelope-island/markers.js
@@ -0,0 +1,16 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4361-155", 41.171506, -112.150672),
+        addGpxMarker(map, "es-4361-166", 41.268256, -112.205881),
+        addGpxMarker(map, "es-4361-169", 41.059294, -112.243400),
+        addGpxMarker(map, "es-4361-199", 41.058669, -112.251350),
+        addGpxMarker(map, "es-4361-201", 41.059036, -112.251586),
+        addGpxMarker(map, "es-4361-202", 41.059353, -112.251914),
+        addGpxMarker(map, "es-4361-203", 41.059414, -112.252006),
+        addGpxMarker(map, "lr-2029", 41.058623, -112.251255),
+        addGpxMarker(map, "es-4361-214", 41.037692, -112.206872),
+        addGpxMarker(map, "es-4361-217", 41.032058, -112.204606),
+        addGpxMarker(map, "es-4361-221", 40.861714, -112.173003),
+        addGpxMarker(map, "es-4361-224", 40.861769, -112.172906),
+    ]
+}
