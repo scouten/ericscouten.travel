3b66ccb2bb9a25a492924fa8b7423912c1a9b85e
Eric Scouten <eric@scouten.com>
2022-09-10 22:42:13 -0700

    Smoketember

---
+++ content/2022/09-10-smoketember/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4408-005", 47.621228, -122.502336),
+        addGpxMarker(map, "es-4408-014", 47.661606, -122.332550),
+        addGpxMarker(map, "es-4408-016", 48.518639, -122.623628),
+        addGpxMarker(map, "es-4408-020", 48.582614, -122.636933),
+        addGpxMarker(map, "es-4408-024", 48.528519, -122.624739),
+        addGpxMarker(map, "es-4408-038", 47.795483, -122.448067),
+        addGpxMarker(map, "es-4408-045", 47.794436, -122.454628),
+    ]
+}
