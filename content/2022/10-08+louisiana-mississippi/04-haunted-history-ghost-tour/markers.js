769f0c4daa836b6f08936ffee1204457e3408c2f
Eric Scouten <git@scouten.me>
2022-11-25 11:50:55 -0800

    Day 4: Haunted History Ghost Tour

---
+++ content/2022/10-08+louisiana-mississippi/04-haunted-history-ghost-tour/markers.js
@@ -0,0 +1,23 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4422-005", 29.957514, -90.065225),
+        addGpxMarker(map, "es-4422-011", 29.958244, -90.065117),
+        addGpxMarker(map, "es-4422-013", 29.958064, -90.064653),
+        addGpxMarker(map, "es-4422-026", 29.958847, -90.064033),
+        addGpxMarker(map, "es-4422-028", 29.959511, -90.063392),
+        addGpxMarker(map, "es-4422-029", 29.959614, -90.063308),
+        addGpxMarker(map, "es-4422-036", 29.959600, -90.063247),
+        addGpxMarker(map, "lr-3103", 29.959478, -90.063469),
+        addGpxMarker(map, "es-4422-042", 29.959661, -90.063208),
+        addGpxMarker(map, "lr-3105", 29.959828, -90.063103),
+        addGpxMarker(map, "es-4422-049", 29.959928, -90.063019),
+        addGpxMarker(map, "es-4422-052", 29.960044, -90.062911),
+        addGpxMarker(map, "es-4422-065", 29.960036, -90.062928),
+        addGpxMarker(map, "es-4422-073", 29.960203, -90.062783),
+        addGpxMarker(map, "es-4422-080", 29.961119, -90.063408),
+        addGpxMarker(map, "es-4422-083", 29.961694, -90.062906),
+        addGpxMarker(map, "es-4422-099", 29.961194, -90.061822),
+        addGpxMarker(map, "es-4422-104", 29.960744, -90.060883),
+        addGpxMarker(map, "es-4422-114", 29.961975, -90.061233),
+    ]
+}
