f32c3cc364e65eb3d690303f3ae68c6459677828
Eric Scouten <git@scouten.me>
2022-12-10 09:45:25 -0800

    Day 7: Two MIssissippis

---
+++ content/2022/10-08+louisiana-mississippi/07-two-mississippis/markers.js
@@ -0,0 +1,14 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4425-042", 29.245533, -89.378381),
+        addGpxMarker(map, "es-4425-076", 29.567314, -89.797006),
+        addGpxMarker(map, "es-4425-079", 30.319415, -89.323537),
+        addGpxMarker(map, "es-4425-081", 30.319415, -89.323537),
+        addGpxMarker(map, "es-4425-104", 30.318653, -89.233658),
+        addGpxMarker(map, "es-4425-110", 30.319158, -89.232306),
+        addGpxMarker(map, "es-4425-136", 30.321258, -89.226539),
+        addGpxMarker(map, "es-4425-145", 30.324119, -89.219589),
+        addGpxMarker(map, "es-4425-146", 30.324094, -89.219581),
+        addGpxMarker(map, "es-4425-153", 30.344722, -89.144858),
+    ]
+}
