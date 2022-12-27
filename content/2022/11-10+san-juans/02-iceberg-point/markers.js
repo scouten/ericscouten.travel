ab70108de00bf9248d1c410291909454fd7ea525
Eric Scouten <git@scouten.me>
2022-12-27 10:01:16 -0800

    Day 2: Iceberg Point

---
+++ content/2022/11-10+san-juans/02-iceberg-point/markers.js
@@ -0,0 +1,16 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4432-078", 48.424872, -122.878806),
+        addGpxMarker(map, "es-4432-080", 48.422347, -122.884522),
+        addGpxMarker(map, "es-4433-004", 48.420666, -122.884125),
+        addGpxMarker(map, "es-4433-009", 48.420282, -122.884150),
+        addGpxMarker(map, "es-4433-014", 48.419810, -122.884840),
+        addGpxMarker(map, "es-4433-021", 48.420156, -122.884923),
+        addGpxMarker(map, "es-4433-022", 48.419907, -122.885756),
+        addGpxMarker(map, "es-4433-024", 48.419983, -122.886049),
+        addGpxMarker(map, "es-4433-031", 48.420850, -122.889418),
+        addGpxMarker(map, "es-4433-036", 48.421054, -122.891997),
+        addGpxMarker(map, "es-4433-038", 48.420937, -122.892267),
+        addGpxMarker(map, "es-4433-040", 48.420903, -122.892372),
+    ]
+}
