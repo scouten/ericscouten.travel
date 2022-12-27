1eb391847c368bd032b3e5334e06a3042ff8c77d
Eric Scouten <git@scouten.me>
2022-12-27 15:16:53 -0800

    SC Day 1: To Edisto Island

---
+++ content/2022/11-28+south-carolina/01-to-edisto-island/markers.js
@@ -0,0 +1,12 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4438-006", 47.334514, -121.003847),
+        addGpxMarker(map, "es-4438-014", 47.299468, -120.811283),
+        addGpxMarker(map, "es-4438-017", 47.294133, -120.782178),
+        addGpxMarker(map, "es-4439-006", 45.718902, -111.543741),
+        addGpxMarker(map, "lr-22b-3445", 32.541819, -80.236436),
+        addGpxMarker(map, "es-4439-031", 32.599350, -80.346375),
+        addGpxMarker(map, "lr-22b-3463", 32.545014, -80.233803),
+        addGpxMarker(map, "lr-22b-3475", 32.547017, -80.232544),
+    ]
+}
---
+++ content/2022/11-28+south-carolina/_index.md
@@ -0,0 +1,7 @@
++++
+title = "South Carolina"
+sort_by = "date"
+transparent = true
++++
+
+Lisa and I spent a week together in South Carolina, visiting friends and exploring Charleston.
