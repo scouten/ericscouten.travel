a6603b3a98d697728dd4a38b573a7ffa275e6349
Eric Scouten <git@scouten.me>
2022-12-28 13:49:12 -0800

    SC Day 2: Hunting Island SP

---
+++ content/2022/11-28+south-carolina/02-hunting-island/markers.js
@@ -0,0 +1,24 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4440-051", 32.350719, -80.459236),
+        addGpxMarker(map, "es-4440-053", 32.351039, -80.459997),
+        addGpxMarker(map, "es-4440-059", 32.351161, -80.461036),
+        addGpxMarker(map, "es-4440-075", 32.351356, -80.463181),
+        addGpxMarker(map, "es-4442-004", 32.351346, -80.462933),
+        addGpxMarker(map, "es-4442-012", 32.351146, -80.460278),
+        addGpxMarker(map, "es-4442-020", 32.351031, -80.459990),
+        addGpxMarker(map, "es-4440-096", 32.350853, -80.459556),
+        addGpxMarker(map, "es-4440-102", 32.344086, -80.456994),
+        addGpxMarker(map, "es-4440-106", 32.344086, -80.456994),
+        addGpxMarker(map, "es-4440-112", 32.343258, -80.455353),
+        addGpxMarker(map, "es-4440-114", 32.343028, -80.454964),
+        addGpxMarker(map, "lr-22b-3514", 32.342878, -80.454856),
+        addGpxMarker(map, "es-4440-121", 32.342850, -80.454781),
+        addGpxMarker(map, "es-4440-133", 32.342453, -80.453094),
+        addGpxMarker(map, "es-4440-140", 32.342539, -80.452789),
+        addGpxMarker(map, "es-4440-152", 32.342669, -80.452378),
+        addGpxMarker(map, "lr-22b-3527", 32.342781, -80.452994),
+        addGpxMarker(map, "lr-22b-3538", 32.342731, -80.454667),
+        addGpxMarker(map, "es-4440-182", 32.342919, -80.454856),
+    ]
+}
