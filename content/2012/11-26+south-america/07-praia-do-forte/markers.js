fb387ff13148985327599eea22497022944bd657
Eric Scouten <git@scouten.me>
2023-01-02 11:33:52 -0800

    South America Day 7: Praia do Forte

---
+++ content/2012/11-26+south-america/07-praia-do-forte/markers.js
@@ -0,0 +1,10 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2245-002", -12.577625, -38.003087),
+        addGpxMarker(map, "es-2245-015", -12.578015, -38.003033),
+        addGpxMarker(map, "es-2245-018a", -12.577740, -38.003240),
+        addGpxMarker(map, "es-2245-020a", -12.577730, -38.003208),
+        addGpxMarker(map, "es-2245-023", -12.652961, -38.068824),
+        addGpxMarker(map, "es-2245-040a", -13.010043, -38.531820),
+    ]
+}
