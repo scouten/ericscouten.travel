1509b343e2c6b0699475841c545d0a57c47172ba
Eric Scouten <git@scouten.me>
2023-01-07 12:18:39 -0800

    Days 14-16: Driving around Uruguay

---
+++ content/2012/11-26+south-america/16-driving-around-uruguay/markers.js
@@ -0,0 +1,13 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2270-002", -34.867054, -55.276756),
+        addGpxMarker(map, "es-2269-001", -34.898493, -55.259505),
+        addGpxMarker(map, "es-2269-004", -34.899097, -55.259801),
+        addGpxMarker(map, "es-2270-004", -34.902526, -55.256980),
+        addGpxMarker(map, "es-2269-011", -34.909137, -55.011947),
+        addGpxMarker(map, "ts-2273-004", -34.906119, -54.823624),
+        addGpxMarker(map, "es-2272-001a", -34.845818, -54.633067),
+        addGpxMarker(map, "es-2273-008", -34.905500, -54.826000),
+        addGpxMarker(map, "es-2273-013", -34.905333, -54.826667),
+    ]
+}
