8e52d3117c8eb7bf60453f9ef141fb9a48f9e728
Eric Scouten <git@scouten.me>
2023-01-07 22:07:53 -0800

    Day 17: Colonia del Sacramento

---
+++ content/2012/11-26+south-america/17-colonia/markers.js
@@ -0,0 +1,17 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2276-011", -34.470595, -57.846028),
+        addGpxMarker(map, "es-2276-015", -34.470466, -57.848431),
+        addGpxMarker(map, "es-2276-021", -34.470636, -57.849444),
+        addGpxMarker(map, "es-2276-023", -34.470619, -57.849480),
+        addGpxMarker(map, "es-2276-037", -34.471095, -57.851373),
+        addGpxMarker(map, "es-2276-040", -34.471091, -57.851413),
+        addGpxMarker(map, "es-2276-043", -34.471151, -57.851644),
+        addGpxMarker(map, "es-2275-007", -34.471248, -57.851758),
+        addGpxMarker(map, "es-2275-008", -34.471337, -57.851793),
+        addGpxMarker(map, "es-2275-009", -34.471703, -57.851815),
+        addGpxMarker(map, "es-2276-051", -34.471432, -57.852000),
+        addGpxMarker(map, "es-2276-064", -34.471097, -57.851178),
+        addGpxMarker(map, "es-2276-077", -34.471064, -57.851117),
+    ]
+}
