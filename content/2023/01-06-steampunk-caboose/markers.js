bfe2713d4c3edc7009f17a396d85ff0808f82c26
Eric Scouten <git@scouten.me>
2023-01-08 16:35:17 -0800

    A Steampunk Caboose!

---
+++ content/2023/01-06-steampunk-caboose/markers.js
@@ -0,0 +1,24 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4467-007", 48.076225, -123.078903),
+        addGpxMarker(map, "es-4467-006", 48.076244, -123.078911),
+        addGpxMarker(map, "es-4466-005", 48.076256, -123.078858),
+        addGpxMarker(map, "es-4466-006", 48.076347, -123.078797),
+        addGpxMarker(map, "es-4466-021", 48.076289, -123.078711),
+        addGpxMarker(map, "es-4467-018", 48.076131, -123.079003),
+        addGpxMarker(map, "es-4466-010", 48.076294, -123.078842),
+        addGpxMarker(map, "es-4467-020", 48.076308, -123.078872),
+        addGpxMarker(map, "es-4467-022", 48.076256, -123.078858),
+        addGpxMarker(map, "es-4467-025", 48.076256, -123.078872),
+        addGpxMarker(map, "es-4466-013", 48.076267, -123.078881),
+        addGpxMarker(map, "es-4466-018", 48.076375, -123.078889),
+        addGpxMarker(map, "es-4466-017", 48.076225, -123.078911),
+        addGpxMarker(map, "es-4466-015", 48.076225, -123.078903),
+        addGpxMarker(map, "es-4467-001", 48.076272, -123.078811),
+        addGpxMarker(map, "es-4467-011", 48.079750, -123.102006),
+        addGpxMarker(map, "es-4467-044", 48.076042, -123.079308),
+        addGpxMarker(map, "es-4467-036", 48.076325, -123.078369),
+        addGpxMarker(map, "es-4467-032", 48.076225, -123.078469),
+        addGpxMarker(map, "es-4467-048", 48.076256, -123.078742),
+    ]
+}
