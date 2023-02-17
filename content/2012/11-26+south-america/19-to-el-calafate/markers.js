3bca435323e48cb489ee859df01e74691219bbf7
Eric Scouten <git@scouten.me>
2023-02-16 22:37:56 -0700

    Day 19: To El Calafate

---
+++ content/2012/11-26+south-america/19-to-el-calafate/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2284-007", -34.586572, -58.391477),
+        addGpxMarker(map, "es-2284-013", -34.586966, -58.393746),
+        addGpxMarker(map, "es-2284-023", -34.586965, -58.393241),
+        addGpxMarker(map, "es-2284-029", -34.587462, -58.392521),
+        addGpxMarker(map, "es-2284-030", -34.587419, -58.391935),
+        addGpxMarker(map, "es-2286-002", -50.284000, -72.053833),
+        addGpxMarker(map, "es-2286-003", -50.335000, -72.230500),
+    ]
+}
