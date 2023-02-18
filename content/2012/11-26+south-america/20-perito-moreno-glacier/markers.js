743acb6f1ccd9a59ec1687193031510e570c6f3d
Eric Scouten <git@scouten.me>
2023-02-17 20:59:06 -0700

    Day 20: Perito Moreno Glacier

---
+++ content/2012/11-26+south-america/20-perito-moreno-glacier/markers.js
@@ -0,0 +1,13 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2288-002", -50.333033, -72.371009),
+        addGpxMarker(map, "es-2290-004", -50.322586, -72.571633),
+        addGpxMarker(map, "es-2289-001", -50.486163, -72.966145),
+        addGpxMarker(map, "es-2290-019", -50.482842, -73.023680),
+        addGpxMarker(map, "es-2290-039", -50.477934, -73.035478),
+        addGpxMarker(map, "es-2290-056", -50.481457, -73.035871),
+        addGpxMarker(map, "es-2289-029a", -50.473277, -73.032610),
+        addGpxMarker(map, "es-2290-067", -50.472802, -73.033789),
+        addGpxMarker(map, "es-2290-072", -50.468842, -73.032279),
+    ]
+}
