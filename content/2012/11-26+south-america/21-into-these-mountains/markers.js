076434464386d1d3d220e83739aead1b13f79720
Eric Scouten <git@scouten.me>
2023-02-18 17:40:10 -0700

    Day 21: Into These Mountains

---
+++ content/2012/11-26+south-america/21-into-these-mountains/markers.js
@@ -0,0 +1,6 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2292-003", -50.334903, -72.230731),
+        addGpxMarker(map, "es-2291-007a", -41.132251, -71.306479),
+    ]
+}
