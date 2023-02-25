35612a25a43a6642c094bde0ad3e95baf392148b
Eric Scouten <git@scouten.me>
2023-02-25 00:11:00 -0800

    Days 27 and 28: Quito … and Scene

---
+++ content/2012/11-26+south-america/27-28-quito-and-scene/markers.js
@@ -0,0 +1,20 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2305-001", -0.228945, -78.518151),
+        addGpxMarker(map, "es-2305-004", -0.225931, -78.519771),
+        addGpxMarker(map, "es-2304-002", -0.225800, -78.519700),
+        addGpxMarker(map, "es-2305-007", -0.223899, -78.514613),
+        addGpxMarker(map, "es-2305-011", -0.225013, -78.513600),
+        addGpxMarker(map, "es-2305-014", -0.224671, -78.512796),
+        addGpxMarker(map, "es-2305-016", -0.220239, -78.515198),
+        addGpxMarker(map, "es-2305-021", -0.220035, -78.515353),
+        addGpxMarker(map, "es-2305-030", -0.220201, -78.514803),
+        addGpxMarker(map, "es-2305-036", -0.220201, -78.514799),
+        addGpxMarker(map, "es-2305-037", -0.220202, -78.514800),
+        addGpxMarker(map, "es-2305-039", -0.220204, -78.514800),
+        addGpxMarker(map, "es-2305-043", -0.220208, -78.514801),
+        addGpxMarker(map, "es-2305-049", -0.220670, -78.513838),
+        addGpxMarker(map, "es-2305-057a", -0.220097, -78.512723),
+        addGpxMarker(map, "es-2306-006", -0.218900, -78.513100),
+    ]
+}
