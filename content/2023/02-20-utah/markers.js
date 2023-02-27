749efb8b8075ba1304d4e3713e82dc58ffb87fb3
Eric Scouten <git@scouten.me>
2023-02-26 17:13:57 -0800

    A Brief Trip to Utah

---
+++ content/2023/02-20-utah/markers.js
@@ -0,0 +1,8 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4482-005", 40.578531, -111.668686),
+        addGpxMarker(map, "es-4482-007", 40.586897, -111.652231),
+        addGpxMarker(map, "es-4482-010", 40.586886, -111.652253),
+        addGpxMarker(map, "es-4483-007", 40.590147, -111.422006),
+    ]
+}
