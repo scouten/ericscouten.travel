5eb329c173d5fcc8ec84bead03220b9a54dd22b9
Eric Scouten <git@scouten.me>
2023-03-11 20:08:24 -0800

    Old-skool blog post for Day 2: Cyprus

---
+++ content/2019/02-cyprus/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3900-004", 35.034453, 32.423896),
+        addGpxMarker(map, "es-3899-009", 35.034980, 32.424253),
+        addGpxMarker(map, "es-3900-006", 35.039337, 32.402002),
+        addGpxMarker(map, "es-3901-003", 35.019720, 32.412879),
+        addGpxMarker(map, "es-3899-012", 34.919522, 32.422267),
+        addGpxMarker(map, "es-3900-011", 34.834261, 32.467735),
+        addGpxMarker(map, "es-3900-013", 34.669555, 32.877637),
+    ]
+}
