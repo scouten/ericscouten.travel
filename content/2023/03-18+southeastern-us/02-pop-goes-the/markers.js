67a519b47e569e8afdc2f730cac446c415fd7c1f
Eric Scouten <git@scouten.me>
2023-03-20 21:45:28 -0400

    Day 2: Pop Goes the …

---
+++ content/2023/03-18+southeastern-us/02-pop-goes-the/markers.js
@@ -0,0 +1,10 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4487-004", 33.199819, -81.690803),
+        addGpxMarker(map, "es-4487-007", 33.010283, -81.306556),
+        addGpxMarker(map, "es-4487-009", 32.565678, -80.281831),
+        addGpxMarker(map, "es-4487-010", 32.495511, -80.346139),
+        addGpxMarker(map, "es-4487-016", 32.720089, -80.348275),
+        addGpxMarker(map, "es-4487-023", 33.702106, -80.240639),
+    ]
+}
