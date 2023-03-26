c526b6a99d7065796d38dcccd9f6c49c46b50158
Eric Scouten <git@scouten.me>
2023-03-25 20:59:55 -0400

    Day 6: Farewell, Wilmington; Hello, Charleston

---
+++ content/2023/03-18+southeastern-us/06-farewell-wilmington-hello-charleston/markers.js
@@ -0,0 +1,20 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4490-022", 34.234333, -77.947167),
+        addGpxMarker(map, "es-4490-025", 34.232667, -77.947000),
+        addGpxMarker(map, "es-4490-026", 34.231833, -77.947000),
+        addGpxMarker(map, "es-4490-030", 34.231333, -77.946833),
+        addGpxMarker(map, "es-4490-031", 34.231000, -77.946833),
+        addGpxMarker(map, "es-4490-036", 34.230667, -77.948333),
+        addGpxMarker(map, "es-4490-040", 34.232167, -77.948500),
+        addGpxMarker(map, "es-4490-050", 34.232667, -77.949500),
+        addGpxMarker(map, "es-4490-061", 32.793833, -79.943500),
+        addGpxMarker(map, "es-4490-067", 32.787500, -79.940333),
+        addGpxMarker(map, "es-4490-069", 32.787167, -79.941333),
+        addGpxMarker(map, "es-4490-070", 32.787167, -79.941500),
+        addGpxMarker(map, "es-4490-071", 32.787000, -79.941833),
+        addGpxMarker(map, "es-4490-073", 32.786667, -79.943167),
+        addGpxMarker(map, "es-4490-077", 32.783500, -79.944167),
+        addGpxMarker(map, "es-4490-078", 32.783000, -79.943667),
+    ]
+}
