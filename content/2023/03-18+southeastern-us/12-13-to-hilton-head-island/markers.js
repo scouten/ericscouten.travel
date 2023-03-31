1e69b8ee052d6ab6504b77b759c0a309a9d6fc03
Eric Scouten <git@scouten.me>
2023-03-30 21:12:55 -0400

    Days 12 and 13: To Hilton Head Island

---
+++ content/2023/03-18+southeastern-us/12-13-to-hilton-head-island/markers.js
@@ -0,0 +1,10 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4497-003", 32.777833, -79.931333),
+        addGpxMarker(map, "es-4497-010", 32.201500, -80.686667),
+        addGpxMarker(map, "es-4497-012", 32.202500, -80.685833),
+        addGpxMarker(map, "es-4497-013", 32.202500, -80.685833),
+        addGpxMarker(map, "es-4497-024", 32.199667, -80.690000),
+        addGpxMarker(map, "es-4497-027", 32.198333, -80.690000),
+    ]
+}
