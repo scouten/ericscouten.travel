ab3cb750ccf0520824cbab88edf9be4fac3d7559
Eric Scouten <git@scouten.me>
2023-04-07 11:35:55 -0400

    Days 16-20: Savannah, Georgia

---
+++ content/2023/03-18+southeastern-us/16-20-savannah-georgia/markers.js
@@ -0,0 +1,19 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4503-006", 32.057333, -81.101333),
+        addGpxMarker(map, "es-4503-007", 32.057167, -81.092500),
+        addGpxMarker(map, "es-4503-010", 32.057167, -81.092167),
+        addGpxMarker(map, "es-4503-011", 32.057500, -81.091833),
+        addGpxMarker(map, "es-4503-015", 32.057167, -81.092000),
+        addGpxMarker(map, "lr-234-4037", 32.079833, -81.084472),
+        addGpxMarker(map, "es-4503-033", 32.079667, -81.084500),
+        addGpxMarker(map, "es-4503-050", 32.056833, -81.092167),
+        addGpxMarker(map, "lr-234-4059", 32.079831, -81.091614),
+        addGpxMarker(map, "es-4503-063", 32.054667, -81.104667),
+        addGpxMarker(map, "es-4504-003", 32.055500, -81.093167),
+        addGpxMarker(map, "es-4504-005", 32.056000, -81.094667),
+        addGpxMarker(map, "es-4504-013", 32.056000, -81.094833),
+        addGpxMarker(map, "es-4504-035", 32.074667, -81.092833),
+        addGpxMarker(map, "es-4504-094", 32.082267, -81.092200),
+    ]
+}
