9e71c301c1015daa8b9af8a8776903dd542e5e82
Eric Scouten <git@scouten.me>
2023-04-08 10:12:00 -0400

    Day 21: Wormsloe State Historic Site

---
+++ content/2023/03-18+southeastern-us/21-wormsloe-state-historic-site/markers.js
@@ -0,0 +1,13 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4505-004", 31.980167, -81.069000),
+        addGpxMarker(map, "es-4505-009", 31.978333, -81.069000),
+        addGpxMarker(map, "lr-234-4074", 31.974044, -81.069139),
+        addGpxMarker(map, "es-4506-011", 31.962520, -81.069384),
+        addGpxMarker(map, "es-4505-013", 31.962500, -81.069333),
+        addGpxMarker(map, "es-4505-018", 31.959667, -81.072667),
+        addGpxMarker(map, "es-4507-007", 31.959991, -81.069587),
+        addGpxMarker(map, "es-4506-025", 31.959760, -81.069244),
+        addGpxMarker(map, "es-4507-013", 31.962142, -81.069331),
+    ]
+}
