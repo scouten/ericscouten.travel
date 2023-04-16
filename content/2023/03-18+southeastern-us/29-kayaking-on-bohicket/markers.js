b552e88e20b718ce0c812a58bbce16bd196d9354
Eric Scouten <git@scouten.me>
2023-04-16 11:32:05 -0400

    Day 29: Kayaking on Bohicket

---
+++ content/2023/03-18+southeastern-us/29-kayaking-on-bohicket/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4518-022", 32.677000, -80.113833),
+        addGpxMarker(map, "es-4518-024", 32.677833, -80.112500),
+        addGpxMarker(map, "es-4518-030", 32.680667, -80.103500),
+        addGpxMarker(map, "es-4518-033", 32.682000, -80.102500),
+        addGpxMarker(map, "es-4518-038", 32.682333, -80.102000),
+        addGpxMarker(map, "es-4518-053", 32.680333, -80.104167),
+        addGpxMarker(map, "es-4518-058", 32.679833, -80.104667),
+    ]
+}
