d48b857546e1adb69ade848ddfe0509c93b0c651
Eric Scouten <git@scouten.me>
2023-04-19 10:28:31 -0700

    Caribbean Days 1 to 3: To the Bahamas

---
+++ content/2017/12-27+caribbean/01-03-to-bahamas/markers.js
@@ -0,0 +1,21 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3661-021", 46.469759, -121.439786),
+        addGpxMarker(map, "es-3662-001", 26.100578, -80.116570),
+        addGpxMarker(map, "es-3662-002", 26.100537, -80.117820),
+        addGpxMarker(map, "es-3662-015", 26.088312, -80.116403),
+        addGpxMarker(map, "es-3664-009", 26.087888, -80.116523),
+        addGpxMarker(map, "es-3664-015", 26.093373, -80.114413),
+        addGpxMarker(map, "es-3663-003", 26.093637, -80.099168),
+        addGpxMarker(map, "es-3662-024", 26.129144, -79.844407),
+        addGpxMarker(map, "es-3665-001", 24.570297, -75.959105),
+        addGpxMarker(map, "es-3665-002", 24.570450, -75.958800),
+        addGpxMarker(map, "es-3665-003", 24.574239, -75.951983),
+        addGpxMarker(map, "es-3666-001", 24.576117, -75.951160),
+        addGpxMarker(map, "es-3665-006", 24.576115, -75.951192),
+        addGpxMarker(map, "es-3666-003", 24.578283, -75.952348),
+        addGpxMarker(map, "es-3666-005", 24.578480, -75.952410),
+        addGpxMarker(map, "es-3666-006", 24.579004, -75.952599),
+        addGpxMarker(map, "es-3666-014", 24.584804, -75.958121),
+    ]
+}
