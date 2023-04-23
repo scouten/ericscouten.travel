934d5bca50a5e60d21c8288a8704af0203ebd65f
Eric Scouten <git@scouten.me>
2023-04-23 15:52:41 -0700

    Days 35 and 36: On the California Coast

---
+++ content/2023/04-18+california/35-36-on-california-coast/markers.js
@@ -0,0 +1,19 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4524-041", 37.872667, -122.456000),
+        addGpxMarker(map, "es-4524-047", 37.873167, -122.456500),
+        addGpxMarker(map, "es-4524-053", 37.872667, -122.456000),
+        addGpxMarker(map, "es-4524-065", 37.887667, -122.554500),
+        addGpxMarker(map, "es-4524-066", 37.900000, -122.563167),
+        addGpxMarker(map, "es-4524-074", 38.247000, -122.904667),
+        addGpxMarker(map, "es-4524-075", 38.474500, -123.158500),
+        addGpxMarker(map, "es-4524-076", 38.474500, -123.158500),
+        addGpxMarker(map, "es-4524-088", 39.298333, -123.795833),
+        addGpxMarker(map, "es-4524-106", 39.306167, -123.800000),
+        addGpxMarker(map, "es-4524-110", 39.304833, -123.804667),
+        addGpxMarker(map, "es-4524-111", 39.303667, -123.804167),
+        addGpxMarker(map, "es-4524-116", 39.304000, -123.801667),
+        addGpxMarker(map, "es-4524-123", 39.304667, -123.799833),
+        addGpxMarker(map, "es-4524-128", 39.303000, -123.806167),
+    ]
+}
