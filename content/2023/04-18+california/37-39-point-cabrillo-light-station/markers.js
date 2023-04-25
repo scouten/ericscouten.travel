f010518eb7bc0b2b16057348de76d9a4d9ffdf2f
Eric Scouten <git@scouten.me>
2023-04-24 23:53:50 -0700

    Days 37-39: Point Cabrillo Light Station

---
+++ content/2023/04-18+california/37-39-point-cabrillo-light-station/markers.js
@@ -0,0 +1,17 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4525-007", 39.307333, -123.799333),
+        addGpxMarker(map, "es-4525-012", 39.306500, -123.800167),
+        addGpxMarker(map, "es-4525-023", 39.304000, -123.801167),
+        addGpxMarker(map, "es-4525-026", 39.304000, -123.801167),
+        addGpxMarker(map, "es-4525-039", 39.350833, -123.822333),
+        addGpxMarker(map, "es-4525-042", 39.351833, -123.825667),
+        addGpxMarker(map, "es-4525-064", 39.350333, -123.825333),
+        addGpxMarker(map, "es-4525-089", 39.348167, -123.825833),
+        addGpxMarker(map, "es-4525-105", 39.349500, -123.821833),
+        addGpxMarker(map, "es-4526-006", 39.357500, -123.626667),
+        addGpxMarker(map, "es-4526-008", 39.357667, -123.618833),
+        addGpxMarker(map, "es-4526-015", 38.979667, -123.116000),
+        addGpxMarker(map, "es-4526-017", 38.979500, -123.116000),
+    ]
+}
