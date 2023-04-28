885a964d1944c65bd4f1fcd85d68d6e62d87c1f2
Eric Scouten <git@scouten.me>
2023-04-28 09:14:48 -0700

    Days 40-42: Fun with the Team

---
+++ content/2023/04-18+california/40-42-fun-with-the-team/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4527-017", 37.331000, -121.894000),
+        addGpxMarker(map, "es-4527-019", 37.331167, -121.893667),
+        addGpxMarker(map, "es-4528-003", 37.043167, -122.063333),
+        addGpxMarker(map, "es-4529-009", 37.037643, -122.062800),
+        addGpxMarker(map, "es-4529-039", 37.042228, -122.054105),
+        addGpxMarker(map, "es-4528-093", 37.041000, -122.054500),
+        addGpxMarker(map, "es-4528-134", 37.039200, -122.059898),
+    ]
+}
