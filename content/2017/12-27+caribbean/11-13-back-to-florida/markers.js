1d45e55a927339480315e5e225b2d70fdd476502
Eric Scouten <git@scouten.me>
2023-05-08 16:30:49 -0700

    Caribbean Days 11-13: Back to Florida

---
+++ content/2017/12-27+caribbean/11-13-back-to-florida/markers.js
@@ -0,0 +1,12 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3696-046", 11.053410, -82.681968),
+        addGpxMarker(map, "es-3697-007", 16.400103, -81.685726),
+        addGpxMarker(map, "es-3694-002", 26.177628, -80.448145),
+        addGpxMarker(map, "es-3699-016", 26.192348, -80.441715),
+        addGpxMarker(map, "es-3699-020", 26.182553, -80.443899),
+        addGpxMarker(map, "es-3694-016", 26.182553, -80.443899),
+        addGpxMarker(map, "es-3694-030", 26.177391, -80.446457),
+        addGpxMarker(map, "es-3694-054", 26.177295, -80.446944),
+    ]
+}
