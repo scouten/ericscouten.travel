e02ab4f56a66f1b1338bb762372b0a0cc5c18d2f
Eric Scouten <git@scouten.me>
2023-05-13 16:40:18 -0700

    Taiwan Day 2: A Midnight Stampede

---
+++ content/2018/03-03+taiwan/02-midnight-stampede/markers.js
@@ -0,0 +1,22 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3711-002", 24.131313, 121.643530),
+        addGpxMarker(map, "es-3711-004", 24.179267, 121.496528),
+        addGpxMarker(map, "es-3709-009", 24.179522, 121.496373),
+        addGpxMarker(map, "es-3709-014", 24.179832, 121.496129),
+        addGpxMarker(map, "es-3709-015", 24.180835, 121.496228),
+        addGpxMarker(map, "es-3711-010", 24.172053, 121.526305),
+        addGpxMarker(map, "es-3711-012", 24.131313, 121.643530),
+        addGpxMarker(map, "es-3709-024", 23.426853, 121.347345),
+        addGpxMarker(map, "es-3710-004", 23.426853, 121.347345),
+        addGpxMarker(map, "es-3711-016", 23.426853, 121.347345),
+        addGpxMarker(map, "es-3709-032", 23.272742, 121.312061),
+        addGpxMarker(map, "es-3709-035", 23.231259, 121.287704),
+        addGpxMarker(map, "es-3711-018", 23.054133, 121.166467),
+        addGpxMarker(map, "es-3711-023", 22.706455, 121.041803),
+        addGpxMarker(map, "es-3709-037", 22.662805, 121.028917),
+        addGpxMarker(map, "es-3709-040", 22.662805, 121.028917),
+        addGpxMarker(map, "es-3711-025", 22.619042, 120.296738),
+        addGpxMarker(map, "es-3712-002", 22.618883, 120.296730),
+    ]
+}
