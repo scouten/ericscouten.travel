2dce6659a0f056d86afcdf302f465c47ede98642
Eric Scouten <git@scouten.me>
2023-05-12 20:20:01 -0700

    Taiwan Day 1: A Birthday Weekend Trip

---
+++ content/2018/03-03+taiwan/01-birthday-weekend-trip/markers.js
@@ -0,0 +1,23 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3707-014", 47.438728, -122.302895),
+        addGpxMarker(map, "es-3707-016", 47.438728, -122.302895),
+        addGpxMarker(map, "es-3707-020", 47.438728, -122.302895),
+        addGpxMarker(map, "es-3708-009", 25.077721, 121.232321),
+        addGpxMarker(map, "es-3706-002", 24.923748, 121.705641),
+        addGpxMarker(map, "es-3706-003", 24.923877, 121.705742),
+        addGpxMarker(map, "es-3706-004", 24.923877, 121.705742),
+        addGpxMarker(map, "es-3706-011", 24.914556, 121.712966),
+        addGpxMarker(map, "es-3706-012", 24.914556, 121.712966),
+        addGpxMarker(map, "es-3706-018", 24.862996, 121.788992),
+        addGpxMarker(map, "es-3708-022", 24.231895, 121.707222),
+        addGpxMarker(map, "es-3708-024", 24.157647, 121.622253),
+        addGpxMarker(map, "es-3706-030", 24.171922, 121.600389),
+        addGpxMarker(map, "es-3706-038", 24.181900, 121.495619),
+        addGpxMarker(map, "es-3706-041", 24.161897, 121.604180),
+        addGpxMarker(map, "es-3708-036", 23.978972, 121.609855),
+        addGpxMarker(map, "es-3708-038", 23.981195, 121.605095),
+        addGpxMarker(map, "es-3708-039", 24.131542, 121.643455),
+        addGpxMarker(map, "es-3708-040", 24.131698, 121.643698),
+    ]
+}
