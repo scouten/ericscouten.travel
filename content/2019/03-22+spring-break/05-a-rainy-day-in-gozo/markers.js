cd9f9480ee0e3917131fe024016add3797e3fcc6
Eric Scouten <git@scouten.me>
2023-05-15 21:54:58 -0700

    Spring Break Day 5: A Rainy Day in Gozo

---
+++ content/2019/03-22+spring-break/05-a-rainy-day-in-gozo/markers.js
@@ -0,0 +1,24 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3910-015", 35.989233, 14.329274),
+        addGpxMarker(map, "es-3910-025", 36.003178, 14.318962),
+        addGpxMarker(map, "es-3910-034", 36.022645, 14.305889),
+        addGpxMarker(map, "es-3910-038", 36.042491, 14.240564),
+        addGpxMarker(map, "es-3910-046", 36.040924, 14.240255),
+        addGpxMarker(map, "es-3910-049", 36.040543, 14.240499),
+        addGpxMarker(map, "es-3910-054", 36.041592, 14.242332),
+        addGpxMarker(map, "es-3910-057", 36.043147, 14.239797),
+        addGpxMarker(map, "es-3910-060", 36.043094, 14.239163),
+        addGpxMarker(map, "es-3910-062", 36.043742, 14.239159),
+        addGpxMarker(map, "es-3910-063", 36.044304, 14.239064),
+        addGpxMarker(map, "es-3910-073", 36.047965, 14.229792),
+        addGpxMarker(map, "es-3910-075", 36.058647, 14.215591),
+        addGpxMarker(map, "es-3910-077", 36.058647, 14.215591),
+        addGpxMarker(map, "es-3910-079", 36.058647, 14.215591),
+        addGpxMarker(map, "es-3910-083", 36.068420, 14.243541),
+        addGpxMarker(map, "es-3910-086", 36.068420, 14.243541),
+        addGpxMarker(map, "es-3910-094", 36.073014, 14.257862),
+        addGpxMarker(map, "es-3910-096", 36.067810, 14.264194),
+        addGpxMarker(map, "es-3912-064", 36.050347, 14.291363),
+    ]
+}
