a48ecbd841156f9ba121c13c9f57b9ce0ec5a9de
Eric Scouten <git@scouten.me>
2023-05-29 18:25:52 -0700

    Spring Break Day 7: Another Night in …

---
+++ content/2019/03-22+spring-break/07e-another-night-in/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3920-001", 36.883094, 10.332247),
+        addGpxMarker(map, "es-3920-004", 36.882694, 10.331967),
+        addGpxMarker(map, "es-3920-008", 36.882229, 10.330805),
+        addGpxMarker(map, "es-3920-010", 36.882846, 10.331084),
+        addGpxMarker(map, "es-3920-012", 36.883709, 10.331819),
+        addGpxMarker(map, "es-3921-004", 36.884098, 10.332000),
+        addGpxMarker(map, "es-3921-006", 36.884272, 10.332000),
+    ]
+}
