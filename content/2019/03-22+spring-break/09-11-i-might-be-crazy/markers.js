7f8589688ecf7bdc73c863ccb9bf8899f4255842
Eric Scouten <git@scouten.me>
2023-06-09 21:41:05 -0700

    Add my power-outlet hack story (ported from FB)

--- content/2019/03-22+spring-break/09-11-i-might-be-crazy/markers.js
+++ content/2019/03-22+spring-break/09-11-i-might-be-crazy/markers.js
@@ -5,6 +5,7 @@ function addGpxMarkers(map) {
         addGpxMarker(map, "es-3921-036", 36.943158, 10.219833),
         addGpxMarker(map, "es-3921-040", 36.935292, 10.189953),
         addGpxMarker(map, "es-3921-047", 35.437145, 19.622280),
+        addGpxMarker(map, "es-3925-002", 55.168918, -139.912531),
         addGpxMarker(map, "as-3935-027", 34.403282, 135.338635),
         addGpxMarker(map, "as-3935-034", 34.446642, 135.381870),
         addGpxMarker(map, "es-3926-006", 34.793798, 135.552698),
