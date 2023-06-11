41b55e9a95fbb922ef92799ba7cdba01b8cd2b68
Eric Scouten <git@scouten.me>
2023-06-11 14:21:25 -0700

    Spring Break Day 15: Tokyo

---
+++ content/2019/03-22+spring-break/15-tokyo/markers.js
@@ -0,0 +1,13 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3937-003", 35.696578, 139.686754),
+        addGpxMarker(map, "es-3936-005", 35.706695, 139.665755),
+        addGpxMarker(map, "es-3937-010", 35.712470, 139.665777),
+        addGpxMarker(map, "es-3936-013", 35.709134, 139.665635),
+        addGpxMarker(map, "es-3936-014", 35.707225, 139.665895),
+        addGpxMarker(map, "es-3937-025", 35.701185, 139.678681),
+        addGpxMarker(map, "es-3937-028", 35.701185, 139.676699),
+        addGpxMarker(map, "es-3937-034", 35.701147, 139.676749),
+        addGpxMarker(map, "es-3940-008", 35.693112, 139.697328),
+    ]
+}
