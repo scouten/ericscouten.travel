a26ad21ba8eed87cbbab6b7cb99141224d32cd26
Eric Scouten <git@scouten.me>
2023-06-21 22:35:01 -0600

    Summer Break Day 2: To Boise

---
+++ content/2023/06-20+summer-break/02-to-boise/markers.js
@@ -0,0 +1,13 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4539-002", 46.291167, -118.557167),
+        addGpxMarker(map, "es-4539-011", 46.289667, -118.510500),
+        addGpxMarker(map, "es-4539-012", 46.278333, -118.222333),
+        addGpxMarker(map, "es-4539-017", 46.270500, -118.155333),
+        addGpxMarker(map, "es-4540-001", 46.407333, -116.802000),
+        addGpxMarker(map, "es-4540-008", 45.524333, -116.304500),
+        addGpxMarker(map, "es-4540-014", 44.966833, -116.181833),
+
+        addParkMarker(map, 118, "Lewis and Clark Trail", "lewis-and-clark-trail", 46.288, -118.072, "es-4539-051"),
+    ]
+}
