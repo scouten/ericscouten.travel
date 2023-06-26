c12be6d70b91880abdf966f5fabb955edfce46dd
Eric Scouten <git@scouten.me>
2023-06-25 22:35:49 -0700

    Summer Break Day 6: Reno, Come Hail or High Water

---
+++ content/2023/06-20+summer-break/06-reno-come-hail-or-high-water/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4543-010", 40.719855, -111.869639),
+        addGpxMarker(map, "es-4543-012", 40.737833, -112.614000),
+        addGpxMarker(map, "es-4543-015", 40.729500, -113.258833),
+        addGpxMarker(map, "es-4543-018", 41.115167, -114.813667),
+        addGpxMarker(map, "es-4543-020", 41.115333, -114.813833),
+        addGpxMarker(map, "es-4543-026", 40.789500, -117.994667),
+        addGpxMarker(map, "es-4543-029", 39.591500, -119.414667),
+    ]
+}
