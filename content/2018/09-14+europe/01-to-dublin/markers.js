b49e02ee24a329ec62ffb196d0a6deea28c9e7e9
Eric Scouten <git@scouten.me>
2023-06-25 21:14:38 -0700

    Europe Day 1: To Dublin

---
+++ content/2018/09-14+europe/01-to-dublin/markers.js
@@ -0,0 +1,12 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3788-019", 40.812548, -74.041414),
+        addGpxMarker(map, "es-3788-026", 41.001158, -73.710130),
+        addGpxMarker(map, "es-3788-031", 53.340645, -6.272755),
+        addGpxMarker(map, "es-3789-001", 53.588445, -6.152585),
+        addGpxMarker(map, "es-3788-095", 53.344070, -6.266683),
+        addGpxMarker(map, "es-3788-097", 53.344000, -6.266700),
+        addGpxMarker(map, "es-3790-005", 53.344180, -6.263558),
+        addGpxMarker(map, "es-3790-006", 53.344298, -6.263700),
+    ]
+}
