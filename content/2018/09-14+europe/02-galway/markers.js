7200bcb20fdd3e323b6fba020938bb8e3cf1afb3
Eric Scouten <git@scouten.me>
2023-06-26 22:32:36 -0700

    Europe Day 2: Galway

---
+++ content/2018/09-14+europe/02-galway/markers.js
@@ -0,0 +1,6 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3791-009", 53.246947, -8.445998),
+        addGpxMarker(map, "es-3793-008", 53.340662, -6.260830),
+    ]
+}
