a0cb7b16fb17eba58a6ea6e339e9227bbb7a6e80
Eric Scouten <git@scouten.me>
2023-07-08 22:09:49 -0700

    Europe Day 10: To Münich

---
+++ content/2018/09-14+europe/10-munich/markers.js
@@ -0,0 +1,8 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3818-002", 51.504958, 0.065620),
+        addGpxMarker(map, "es-3818-004", 51.578828, 0.573645),
+        addGpxMarker(map, "es-3818-007", 51.353237, 0.969747),
+        addGpxMarker(map, "es-3819-001", 50.249108, 8.623145),
+    ]
+}
