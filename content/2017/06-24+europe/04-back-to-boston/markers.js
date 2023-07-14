75d9fffcc4f92ea8ed5a0f33ed04653a251dcc6b
Eric Scouten <git@scouten.me>
2023-07-14 08:38:28 -0700

    Europe Day 4: Back to Boston

---
+++ content/2017/06-24+europe/04-back-to-boston/markers.js
@@ -0,0 +1,8 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3488-001", 44.698652, -73.374449),
+        addGpxMarker(map, "es-3487-009", 44.698538, -73.374198),
+        addGpxMarker(map, "es-3488-013", 44.696485, -73.370148),
+        addGpxMarker(map, "es-3487-014", 44.693152, -73.363227),
+    ]
+}
