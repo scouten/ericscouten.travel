ea898a8d9445cb64f28f8e7861d7c532d9526c76
Eric Scouten <git@scouten.me>
2023-07-22 17:42:32 -0700

    Europe Day 11: Flight to Dublin

---
+++ content/2017/06-24+europe/11-flight-to-dublin/markers.js
@@ -0,0 +1,16 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3507-004", 55.736897, -3.332797),
+        addGpxMarker(map, "es-3507-007", 55.744022, -3.345587),
+        addGpxMarker(map, "es-3507-015", 55.982014, -3.315410),
+        addGpxMarker(map, "es-3507-027", 55.234947, -4.136260),
+        addGpxMarker(map, "es-3507-036", 54.737453, -4.921974),
+        addGpxMarker(map, "es-3507-041", 54.319720, -5.405943),
+        addGpxMarker(map, "es-3507-043", 54.234953, -5.486918),
+        addGpxMarker(map, "es-3507-050", 53.572136, -6.365051),
+        addGpxMarker(map, "es-3507-051", 53.588065, -6.397187),
+        addGpxMarker(map, "es-3507-061", 53.462496, -6.576540),
+        addGpxMarker(map, "es-3507-065", 53.433870, -6.510813),
+        addGpxMarker(map, "es-3507-077", 53.427597, -6.239763),
+    ]
+}
