159a28efe8e980105bc3feffade7b5b2d6477bc2
Eric Scouten <git@scouten.me>
2023-07-25 22:34:13 -0700

    Europe Day 13: And Now We Are Three

---
+++ content/2017/06-24+europe/13-and-now-we-are-three/markers.js
@@ -0,0 +1,10 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3512-001", 53.346136, -6.240614),
+        addGpxMarker(map, "es-3513-006", 53.348087, -6.260137),
+        addGpxMarker(map, "es-3513-007", 53.346772, -6.263242),
+        addGpxMarker(map, "es-3512-014", 53.346068, -6.263156),
+        addGpxMarker(map, "es-3513-011", 53.344363, -6.263225),
+        addGpxMarker(map, "es-3512-017", 53.345306, -6.255837),
+    ]
+}
