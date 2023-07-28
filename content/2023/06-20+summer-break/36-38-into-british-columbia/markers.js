c7feda873633a057067ddf28b680094dcdb14485
Eric Scouten <git@scouten.me>
2023-07-27 23:51:42 -0700

    Summer Break Resumes: Into British Columbia

---
+++ content/2023/06-20+summer-break/36-38-into-british-columbia/markers.js
@@ -0,0 +1,14 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4561-004", 49.274833, -123.127500),
+        addGpxMarker(map, "es-4561-012", 49.239333, -123.129500),
+        addGpxMarker(map, "es-4561-013", 49.239333, -123.129333),
+        addGpxMarker(map, "es-4561-022", 49.238000, -123.132500),
+        addGpxMarker(map, "es-4562-010", 49.239986, -123.130962),
+        addGpxMarker(map, "es-4564-015", 50.385833, -122.373667),
+        addGpxMarker(map, "es-4564-027", 50.415833, -122.280333),
+        addGpxMarker(map, "es-4564-033", 50.668167, -121.986833),
+        addGpxMarker(map, "es-4564-034", 50.748500, -121.849167),
+        addGpxMarker(map, "es-4564-036", 50.838667, -121.700500),
+    ]
+}
