a4e21a5f6700ded39c27398db0605ce065ffa9b1
Eric Scouten <git@scouten.me>
2023-07-27 19:19:37 -0700

    Europe Day 14: Around Dublin

---
+++ content/2017/06-24+europe/14-around-dublin/markers.js
@@ -0,0 +1,19 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3515-012", 53.338952, -6.252721),
+        addGpxMarker(map, "es-3515-017", 53.338377, -6.253335),
+        addGpxMarker(map, "es-3515-046", 53.342978, -6.271259),
+        addGpxMarker(map, "es-3515-051", 53.338843, -6.272244),
+        addGpxMarker(map, "es-3515-058", 53.339416, -6.280114),
+        addGpxMarker(map, "es-3515-068", 53.343303, -6.292578),
+        addGpxMarker(map, "es-3515-082", 53.346965, -6.281007),
+        addGpxMarker(map, "es-3515-102", 53.347437, -6.255226),
+        addGpxMarker(map, "es-3517-003", 53.384991, -6.065065),
+        addGpxMarker(map, "ts-3519-004", 53.387662, -6.065122),
+        addGpxMarker(map, "ts-3519-008", 53.387725, -6.065122),
+        addGpxMarker(map, "es-3517-020", 53.388468, -6.063736),
+        addGpxMarker(map, "es-3517-021", 53.388391, -6.063872),
+        addGpxMarker(map, "es-3517-032", 53.387986, -6.064801),
+        addGpxMarker(map, "es-3517-033", 53.387893, -6.064894),
+    ]
+}
