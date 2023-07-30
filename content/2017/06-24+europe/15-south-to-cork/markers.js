daf59c61cf522c31716eddbb615049c0a4950122
Eric Scouten <git@scouten.me>
2023-07-29 17:39:27 -0700

    Europe Day 15: South to Cork

---
+++ content/2017/06-24+europe/15-south-to-cork/markers.js
@@ -0,0 +1,7 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3520-001", 52.980525, -6.040226),
+        addGpxMarker(map, "es-3520-004", 52.980525, -6.040226),
+        addGpxMarker(map, "es-3520-007", 52.980525, -6.040226),
+    ]
+}
