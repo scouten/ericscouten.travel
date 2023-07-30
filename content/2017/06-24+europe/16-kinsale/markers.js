9c5a5eaed06edda472ad8c7407c5f18468075689
Eric Scouten <git@scouten.me>
2023-07-29 23:17:22 -0700

    Europe Day 16: Kinsale

---
+++ content/2017/06-24+europe/16-kinsale/markers.js
@@ -0,0 +1,18 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3524-001", 51.757949, -8.545281),
+        addGpxMarker(map, "ts-3526-023", 51.705707, -8.519847),
+        addGpxMarker(map, "es-3524-005", 51.705539, -8.519262),
+        addGpxMarker(map, "es-3524-032", 51.704576, -8.522329),
+        addGpxMarker(map, "es-3524-044", 51.703974, -8.521830),
+        addGpxMarker(map, "ts-3526-039", 51.702308, -8.520520),
+        addGpxMarker(map, "es-3525-010", 51.702637, -8.519139),
+        addGpxMarker(map, "es-3524-061", 51.703052, -8.519484),
+        addGpxMarker(map, "es-3524-072", 51.703874, -8.520233),
+        addGpxMarker(map, "es-3524-075", 51.703874, -8.520233),
+        addGpxMarker(map, "es-3524-078p", 51.703874, -8.520233),
+        addGpxMarker(map, "es-3524-091", 51.705298, -8.516276),
+        addGpxMarker(map, "es-3524-102", 51.706119, -8.509609),
+        addGpxMarker(map, "es-3524-104", 51.737195, -8.483735),
+    ]
+}
