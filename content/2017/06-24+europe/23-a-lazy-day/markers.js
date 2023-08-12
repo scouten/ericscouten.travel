60eecdd94b2f44a359364fe107aad0ae0c11b131
Eric Scouten <git@scouten.me>
2023-08-12 16:09:04 -0700

    Europe Day 23: A Lazy Day

---
+++ content/2017/06-24+europe/23-a-lazy-day/markers.js
@@ -0,0 +1,22 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3553-005", 58.027656, 7.454354),
+        addGpxMarker(map, "es-3553-006", 58.027817, 7.454941),
+        addGpxMarker(map, "es-3553-007", 58.027817, 7.454941),
+        addGpxMarker(map, "es-3553-010", 58.027932, 7.455292),
+        addGpxMarker(map, "es-3553-011", 58.027916, 7.455701),
+        addGpxMarker(map, "es-3553-015", 58.027572, 7.456403),
+        addGpxMarker(map, "es-3553-017", 58.027484, 7.455664),
+        addGpxMarker(map, "es-3553-019", 58.027368, 7.455246),
+        addGpxMarker(map, "es-3553-020", 58.027255, 7.454758),
+        addGpxMarker(map, "es-3553-023", 58.027397, 7.453878),
+        addGpxMarker(map, "ts-3555-008", 58.027217, 7.452703),
+        addGpxMarker(map, "es-3553-027", 58.027185, 7.452813),
+        addGpxMarker(map, "es-3553-029", 58.033442, 7.504683),
+        addGpxMarker(map, "es-3553-032", 58.033442, 7.504683),
+        addGpxMarker(map, "es-3553-033", 58.010427, 7.535527),
+        addGpxMarker(map, "es-3556-008", 58.010156, 7.547208),
+        addGpxMarker(map, "es-3556-011", 58.010156, 7.547208),
+        addGpxMarker(map, "es-3556-018", 58.010156, 7.547208),
+    ]
+}
