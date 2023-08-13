c622fdf7fb4f6060cc501e56bd2576460c47a5ad
Eric Scouten <git@scouten.me>
2023-08-12 22:31:02 -0700

    Europe Day 24: On a Boat Again

---
+++ content/2017/06-24+europe/24-on-a-boat-again/markers.js
@@ -0,0 +1,18 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3557-001", 58.104039, 7.664095),
+        addGpxMarker(map, "es-3559-001", 57.988391, 7.590895),
+        addGpxMarker(map, "es-3559-003", 57.988377, 7.590932),
+        addGpxMarker(map, "es-3559-013", 57.988347, 7.591184),
+        addGpxMarker(map, "es-3559-015", 57.989281, 7.583295),
+        addGpxMarker(map, "es-3559-019", 57.994753, 7.568217),
+        addGpxMarker(map, "es-3559-023", 57.996228, 7.569152),
+        addGpxMarker(map, "es-3559-034", 58.007654, 7.560287),
+        addGpxMarker(map, "es-3559-039", 58.006097, 7.549161),
+        addGpxMarker(map, "es-3559-046", 58.006028, 7.531981),
+        addGpxMarker(map, "es-3559-053", 58.005796, 7.544973),
+        addGpxMarker(map, "es-3559-061", 58.005562, 7.551395),
+        addGpxMarker(map, "es-3559-067", 58.008475, 7.553725),
+        addGpxMarker(map, "es-3559-072", 58.008687, 7.547229),
+    ]
+}
