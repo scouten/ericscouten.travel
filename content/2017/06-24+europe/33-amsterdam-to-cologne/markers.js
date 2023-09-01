3893e5878787b7722f03653152039a8a520f4320
Eric Scouten <git@scouten.me>
2023-09-01 11:21:42 -0700

    Europe Day 33: Amsterdam to Cologne

---
+++ content/2017/06-24+europe/33-amsterdam-to-cologne/markers.js
@@ -0,0 +1,17 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3585-005", 52.365708, 4.876433),
+        addGpxMarker(map, "es-3585-010", 52.361663, 4.884372),
+        addGpxMarker(map, "es-3585-007", 50.937080, 6.960817),
+        addGpxMarker(map, "es-3586-005n", 50.941323, 6.957026),
+        addGpxMarker(map, "es-3586-011n", 50.941308, 6.957039),
+        addGpxMarker(map, "es-3586-014n", 50.941302, 6.957044),
+        addGpxMarker(map, "es-3586-017n", 50.941295, 6.957051),
+        addGpxMarker(map, "es-3586-024n", 50.941282, 6.957063),
+        addGpxMarker(map, "es-3586-027", 50.941273, 6.957071),
+        addGpxMarker(map, "es-3586-028", 50.941271, 6.957072),
+        addGpxMarker(map, "es-3586-029", 50.941274, 6.957052),
+        addGpxMarker(map, "es-3586-034", 50.941278, 6.956417),
+        addGpxMarker(map, "es-3586-038", 50.941215, 6.956564),
+    ]
+}
