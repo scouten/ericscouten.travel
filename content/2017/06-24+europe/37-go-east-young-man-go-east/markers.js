daaa97629046a87fa8c48ee54b177bea07d4ea1b
Eric Scouten <git@scouten.me>
2023-09-04 21:48:23 -0700

    Europe Day 37: Go East, Young Man, Go East!

---
+++ content/2017/06-24+europe/37-go-east-young-man-go-east/markers.js
@@ -0,0 +1,7 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3590-004", 41.800974, 12.273403),
+        addGpxMarker(map, "es-3591-003", 45.077194, 25.865009),
+        addGpxMarker(map, "es-3591-004", 45.077194, 25.865009),
+    ]
+}
