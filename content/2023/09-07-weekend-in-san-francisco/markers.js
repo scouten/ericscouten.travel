2d7ce25cfc1476a329796234da34dbf7e77d546b
Eric Scouten <git@scouten.me>
2023-09-10 16:20:36 -0700

    A Weekend in San Francisco

---
+++ content/2023/09-07-weekend-in-san-francisco/markers.js
@@ -0,0 +1,24 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4572-003", 37.787000, -122.404500),
+        addGpxMarker(map, "lr-239-4579", 37.789825, -122.401794),
+        addGpxMarker(map, "es-4572-013", 37.786833, -122.404667),
+        addGpxMarker(map, "es-4572-016", 37.785000, -122.407667),
+        addGpxMarker(map, "es-4572-017", 37.784798, -122.407600),
+        addGpxMarker(map, "es-4572-019", 37.784833, -122.407833),
+        addGpxMarker(map, "es-4572-022", 37.785500, -122.407833),
+        addGpxMarker(map, "es-4572-027", 37.790500, -122.408833),
+        addGpxMarker(map, "es-4572-033", 37.794667, -122.409667),
+        addGpxMarker(map, "es-4573-002", 37.770833, -122.401500),
+        addGpxMarker(map, "es-4573-008", 37.790000, -122.399667),
+        addGpxMarker(map, "es-4573-009", 37.789900, -122.399798),
+        addGpxMarker(map, "lr-239-4589", 37.790139, -122.399925),
+        addGpxMarker(map, "es-4573-010", 37.795167, -122.392167),
+        addGpxMarker(map, "es-4573-014", 37.811333, -122.410833),
+        addGpxMarker(map, "lr-239-4591", 37.811353, -122.410850),
+        addGpxMarker(map, "lr-239-4592", 37.811358, -122.410867),
+        addGpxMarker(map, "es-4574-003", 37.786167, -122.406333),
+        addGpxMarker(map, "es-4574-007", 37.644653, -122.346672),
+        addGpxMarker(map, "es-4574-010", 37.662491, -122.315252),
+    ]
+}
