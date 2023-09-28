14601fe9f0470c9dcc8157e2be91fbb6ea47cd3c
Eric Scouten <git@scouten.me>
2023-09-27 18:14:16 -0700

    Europe Day 42: Austrian Countryside and Vienna

---
+++ content/2017/06-24+europe/42-austrian-countryside-and-vienna/markers.js
@@ -0,0 +1,21 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3611-002", 47.799920, 13.046962),
+        addGpxMarker(map, "es-3610-003", 47.847427, 13.439384),
+        addGpxMarker(map, "es-3610-009", 47.839622, 13.455207),
+        addGpxMarker(map, "es-3610-015", 47.839622, 13.455207),
+        addGpxMarker(map, "es-3610-019", 47.839688, 13.457798),
+        addGpxMarker(map, "es-3610-021", 47.853566, 13.483347),
+        addGpxMarker(map, "es-3610-026", 47.847817, 13.475579),
+        addGpxMarker(map, "es-3610-030", 47.847817, 13.475579),
+        addGpxMarker(map, "es-3610-031", 47.847817, 13.475579),
+        addGpxMarker(map, "es-3612-005", 48.211545, 16.359491),
+        addGpxMarker(map, "es-3612-010", 48.211435, 16.358554),
+        addGpxMarker(map, "es-3612-013", 48.208212, 16.359325),
+        addGpxMarker(map, "es-3612-015", 48.208020, 16.359255),
+        addGpxMarker(map, "es-3612-019", 48.208051, 16.359223),
+        addGpxMarker(map, "es-3612-021", 48.207010, 16.359514),
+        addGpxMarker(map, "es-3612-022", 48.206736, 16.359994),
+        addGpxMarker(map, "es-3612-023", 48.207292, 16.362256),
+    ]
+}
