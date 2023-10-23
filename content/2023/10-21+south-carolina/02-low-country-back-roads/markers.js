0f238879a7fc9464735b0e7c2fa43c52167fc8e5
Eric Scouten <git@scouten.me>
2023-10-22 21:52:57 -0400

    South Carolina Day 2: Low Country Back Roads

---
+++ content/2023/10-21+south-carolina/02-low-country-back-roads/markers.js
@@ -0,0 +1,10 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4600-013", 32.591000, -80.463000),
+        addGpxMarker(map, "es-4600-037p", 32.591847, -80.468750),
+        addGpxMarker(map, "es-4600-052", 32.592269, -80.468994),
+        addGpxMarker(map, "es-4600-067", 32.611953, -80.480819),
+        addGpxMarker(map, "es-4600-071", 32.612022, -80.480981),
+        addGpxMarker(map, "es-4600-072", 32.697797, -80.402078),
+    ]
+}
