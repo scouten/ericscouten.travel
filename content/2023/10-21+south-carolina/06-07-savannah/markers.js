634858803cfd7802ab30494da92c82bc82a8149d
Eric Scouten <git@scouten.me>
2023-10-29 22:59:01 -0400

    Days 6 and 7: Savannah

---
+++ content/2023/10-21+south-carolina/06-07-savannah/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4604-334", 32.068000, -81.099833),
+        addGpxMarker(map, "es-4604-338", 32.069500, -81.098833),
+        addGpxMarker(map, "es-4604-346", 32.072167, -81.097167),
+        addGpxMarker(map, "es-4604-356", 32.075833, -81.093167),
+        addGpxMarker(map, "es-4604-372", 32.070500, -81.094833),
+        addGpxMarker(map, "es-4604-379", 32.069000, -81.097000),
+        addGpxMarker(map, "es-4608-003", 32.068167, -81.100000),
+        addGpxMarker(map, "es-4604-388", 32.083667, -81.095833),
+        addGpxMarker(map, "es-4604-391", 32.083500, -81.095500),
+        addGpxMarker(map, "es-4604-395", 32.083667, -81.095833),
+        addGpxMarker(map, "lr-23a-4664", 32.083692, -81.096175),
+    ]
+}
