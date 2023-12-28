51a4cc89dcd5c17c3b4180653859f1557ea27c2c
Eric Scouten <git@scouten.me>
2023-12-27 19:30:58 -0800

    Vancouver Island Day 1: Coho Ferry Crossing

---
+++ content/2023/12-27+vancouver-island/01-coho-ferry-crossing/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4622-013", 48.121500, -123.430833),
+        addGpxMarker(map, "es-4622-056", 48.363667, -123.386500),
+        addGpxMarker(map, "es-4622-064", 48.385167, -123.376167),
+        addGpxMarker(map, "es-4622-070", 48.405000, -123.391167),
+        addGpxMarker(map, "es-4622-075", 48.412000, -123.396167),
+        addGpxMarker(map, "es-4622-080", 48.413667, -123.397000),
+        addGpxMarker(map, "es-4622-102", 48.567500, -123.546833),
+    ]
+}
---
+++ content/2023/12-27+vancouver-island/_index.md
@@ -0,0 +1,10 @@
++++
+title = "Vancouver Island"
+sort_by = "date"
+transparent = true
+
+[extra]
+# cover_abs = "/2023/04-18+california/37-39-point-cabrillo-light-station/cover-es-4525-064.jpg"
++++
+
+I'm exploring Vancouver Island beyond Victoria.
