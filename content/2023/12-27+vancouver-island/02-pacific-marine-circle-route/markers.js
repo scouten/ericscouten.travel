edbfc23e71fd95ad430a6fc12f717662ade18ceb
Eric Scouten <git@scouten.me>
2023-12-28 20:20:25 -0800

    Vancouver Island Day 2: Pacific Marine Circle Route

---
+++ content/2023/12-27+vancouver-island/02-pacific-marine-circle-route/markers.js
@@ -0,0 +1,16 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4623-006p", 48.875667, -124.220167),
+        addGpxMarker(map, "es-4623-013", 48.875667, -124.220000),
+        addGpxMarker(map, "es-4623-016", 48.807333, -124.127500),
+        addGpxMarker(map, "es-4623-017", 48.724333, -124.134833),
+        addGpxMarker(map, "es-4623-024", 48.634833, -124.249500),
+        addGpxMarker(map, "es-4623-027", 48.635000, -124.249667),
+        addGpxMarker(map, "es-4623-029", 48.632333, -124.248500),
+        addGpxMarker(map, "es-4623-040", 48.561167, -124.400000),
+        addGpxMarker(map, "es-4623-047", 48.422500, -124.058300),
+        addGpxMarker(map, "es-4623-050", 48.422500, -124.058333),
+        addGpxMarker(map, "es-4623-060", 48.422500, -124.058333),
+        addGpxMarker(map, "es-4623-063", 48.422500, -124.058333),
+    ]
+}
