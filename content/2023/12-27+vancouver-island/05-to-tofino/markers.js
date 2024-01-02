06a6d2b264b834a84e2213620bc328e468b1ff38
Eric Scouten <git@scouten.me>
2024-01-01 17:28:53 -0800

    Vancouver Island Day 5: To Tofino

---
+++ content/2023/12-27+vancouver-island/05-to-tofino/markers.js
@@ -0,0 +1,14 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4626-003", 50.714167, -127.493667),
+        addGpxMarker(map, "es-4627-004", 50.714282, -127.493277),
+        addGpxMarker(map, "es-4627-015", 50.705188, -127.482840),
+        addGpxMarker(map, "es-4626-009", 50.410500, -126.961500),
+        addGpxMarker(map, "es-4626-017", 50.329000, -126.905167),
+        addGpxMarker(map, "es-4626-032", 49.278833, -125.357333),
+        addGpxMarker(map, "es-4626-040", 49.116667, -125.438500),
+        addGpxMarker(map, "es-4627-023", 49.116650, -125.438518),
+        addGpxMarker(map, "es-4626-048", 49.047667, -125.502000),
+        addGpxMarker(map, "es-4626-052", 49.047667, -125.502000),
+    ]
+}
