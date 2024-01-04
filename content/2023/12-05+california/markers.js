2ff56b4ae90d8121c84b99e614583bf62613c29e
Eric Scouten <git@scouten.me>
2024-01-03 19:50:51 -0800

    More detail on Dec '23 Calif trip

--- content/2023/12-05+california/markers.js
+++ content/2023/12-05+california/markers.js
@@ -1,6 +1,7 @@
 function addGpxMarkers(map) {
     return [
         addGpxMarker(map, "es-4617-030", 47.463667, -122.306333),
+        addGpxMarker(map, "es-4618-014", 37.331167, -121.892833),
         addGpxMarker(map, "es-4620-026", 37.333667, -121.890833),
         addGpxMarker(map, "es-4620-043", 37.913760, -121.987338),
         addGpxMarker(map, "es-4620-046", 41.332347, -121.934763),
