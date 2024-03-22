042a64d4124bf874ca57f235251c08e7c64706c7
Eric Scouten <git@scouten.me>
2024-03-21 19:55:32 -0700

    Lake Crescent

---
+++ content/2021/01-30-lake-crescent/markers.js
@@ -0,0 +1,10 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4167-014", 48.072917, -123.773438),
+        addGpxMarker(map, "es-4167-022", 48.049403, -123.856400),
+        addGpxMarker(map, "es-4167-074", 48.063367, -123.908600),
+        addGpxMarker(map, "es-4167-083", 48.063408, -123.908645),
+
+        addParkMarker(map, 18, "Bogachiel", "bogachiel", 47.8937, -124.3627, "es-4167-049"),
+    ]
+}
