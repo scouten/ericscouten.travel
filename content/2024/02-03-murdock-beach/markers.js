c970a6dca1752ff6ff945483f4981f44ca72fb0b
Eric Scouten <git@scouten.me>
2024-03-24 10:33:55 -0700

    Murdock Beach

---
+++ content/2024/02-03-murdock-beach/markers.js
@@ -0,0 +1,17 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4630-034", 48.154333, -123.865333),
+        addGpxMarker(map, "es-4630-037", 48.154333, -123.865167),
+        addGpxMarker(map, "es-4630-038", 48.154333, -123.865167),
+        addGpxMarker(map, "es-4630-041", 48.154333, -123.865167),
+        addGpxMarker(map, "es-4630-046", 48.154333, -123.865333),
+        addGpxMarker(map, "es-4630-049", 48.154500, -123.865333),
+        addGpxMarker(map, "es-4630-053", 48.154500, -123.865500),
+        addGpxMarker(map, "es-4630-059", 48.154500, -123.865500),
+        addGpxMarker(map, "es-4630-068", 48.154500, -123.865667),
+        addGpxMarker(map, "es-4630-072", 48.154500, -123.865667),
+        addGpxMarker(map, "es-4630-081", 48.154333, -123.864833),
+        addGpxMarker(map, "es-4630-087", 48.154333, -123.864167),
+        addGpxMarker(map, "es-4630-089", 48.154333, -123.864167),
+    ]
+}
