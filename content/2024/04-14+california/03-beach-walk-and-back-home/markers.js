7fcd0931ec26ca815f30905b91092e7fe7c3161d
Eric Scouten <git@scouten.me>
2024-05-06 21:07:31 -0700

    California Part 3: A Beach Walk and Back to Home

---
+++ content/2024/04-14+california/03-beach-walk-and-back-home/markers.js
@@ -0,0 +1,17 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4642-043", 39.306000, -123.809500),
+        addGpxMarker(map, "es-4642-052", 39.307000, -123.810000),
+        addGpxMarker(map, "es-4642-058", 39.309500, -123.810667),
+        addGpxMarker(map, "es-4642-061", 39.309500, -123.810700),
+        addGpxMarker(map, "es-4642-073", 39.310667, -123.808333),
+        addGpxMarker(map, "es-4642-076", 39.310667, -123.808167),
+        addGpxMarker(map, "es-4642-078", 39.310500, -123.807667),
+        addGpxMarker(map, "es-4642-079", 39.311333, -123.806333),
+        addGpxMarker(map, "es-4642-084", 39.312333, -123.803833),
+        addGpxMarker(map, "es-4642-088", 39.312333, -123.803667),
+        addGpxMarker(map, "es-4642-089", 39.312833, -123.803667),
+        addGpxMarker(map, "es-4642-097", 39.313600, -123.804600),
+        addGpxMarker(map, "es-4643-018", 43.919145, -122.138653),
+    ]
+}
