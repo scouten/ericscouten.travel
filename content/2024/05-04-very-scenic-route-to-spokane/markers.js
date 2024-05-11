ebeba2aa5002e0abb7244581e9364352f73a32ea
Eric Scouten <git@scouten.me>
2024-05-10 21:49:56 -0700

    The Very Scenic Route to Spokane

---
+++ content/2024/05-04-very-scenic-route-to-spokane/markers.js
@@ -0,0 +1,22 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4644-052", 45.586500, -117.733167),
+        addGpxMarker(map, "es-4644-066", 45.570155, -117.527017),
+        addGpxMarker(map, "es-4644-068", 45.429547, -117.278808),
+        addGpxMarker(map, "es-4644-070", 45.456937, -117.281608),
+        addGpxMarker(map, "es-4644-072", 45.522708, -117.261445),
+        addGpxMarker(map, "es-4644-080", 45.835167, -117.263667),
+        addGpxMarker(map, "es-4644-110", 45.974333, -117.272167),
+        addGpxMarker(map, "es-4644-119", 45.982000, -117.272833),
+        addGpxMarker(map, "es-4644-138", 46.051833, -117.238500),
+        addGpxMarker(map, "es-4644-139", 46.050333, -117.237167),
+        addGpxMarker(map, "es-4644-145", 46.047667, -117.239833),
+        addGpxMarker(map, "es-4644-147", 46.047500, -117.239833),
+        addGpxMarker(map, "es-4644-149", 46.052833, -117.233333),
+        addGpxMarker(map, "es-4644-233", 46.137333, -117.131333),
+
+        addParkMarker(map, 127, "Fields Spring", "fields-spring", 46.075, -117.172, "es-4644-193p"),
+        addParkMarker(map, 128, "Spokane Plains Battlefield", "spokane-plains-battlefield", 47.643, -117.648, "es-4645-011"),
+        addParkMarker(map, 129, "Riverside", "riverside", 47.750, -117.541, "es-4645-085"),
+    ]
+}
