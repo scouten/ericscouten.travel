7813bf65e396ea24e084340f2f18aa19226898e3
Eric Scouten <git@scouten.me>
2024-05-17 21:04:25 -0400

    East Coast Part 1: To “The Other Washington”

---
+++ content/2024/05-12+east-coast/01-to-the-other-washington/markers.js
@@ -0,0 +1,12 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4647-012", 41.670325, -87.881683),
+        addGpxMarker(map, "es-4647-023", 38.885500, -77.006000),
+        addGpxMarker(map, "es-4647-027", 38.886000, -77.005333),
+        addGpxMarker(map, "es-4647-039", 38.886167, -77.002000),
+        addGpxMarker(map, "es-4647-045", 38.887500, -77.007000),
+        addGpxMarker(map, "es-4647-049", 38.889667, -77.022333),
+        addGpxMarker(map, "es-4647-073", 38.894667, -77.003500),
+        addGpxMarker(map, "es-4647-075", 38.894667, -77.003500),
+    ]
+}
