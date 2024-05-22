6c4f809be5b0602ab5bf58b475c5a4daf01248b9
Eric Scouten <git@scouten.me>
2024-05-22 12:25:33 -0700

    East Coast Part 4: Around New York City

---
+++ content/2024/05-12+east-coast/04-around-new-york-city/markers.js
@@ -0,0 +1,16 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4647-227", 40.740500, -74.007833),
+        addGpxMarker(map, "es-4647-233", 40.737500, -74.005833),
+        addGpxMarker(map, "es-4648-008", 40.737667, -73.997167),
+        addGpxMarker(map, "es-4650-033", 40.745000, -73.990167),
+        addGpxMarker(map, "es-4650-036", 40.744333, -73.994333),
+        addGpxMarker(map, "es-4650-040", 40.744333, -73.996167),
+        addGpxMarker(map, "es-4650-044", 40.740833, -74.001667),
+        addGpxMarker(map, "es-4650-053", 40.740833, -74.007500),
+        addGpxMarker(map, "es-4652-007", 40.737667, -73.992500),
+        addGpxMarker(map, "es-4652-028", 40.741167, -74.008000),
+        addGpxMarker(map, "es-4653-008", 40.737000, -73.991833),
+        addGpxMarker(map, "es-4654-127", 40.740667, -74.008167),
+    ]
+}
