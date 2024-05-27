2a71eb3598dc8c01faa80792c0920d1d923dfa6d
Eric Scouten <git@scouten.me>
2024-05-26 21:34:14 -0700

    East Coast Part 6: Back to DC

---
+++ content/2024/05-12+east-coast/06-back-to-dc/markers.js
@@ -0,0 +1,14 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4655-098", 40.746500, -74.005833),
+        addGpxMarker(map, "es-4655-100", 40.746500, -74.005833),
+        addGpxMarker(map, "es-4655-101", 40.748000, -74.004667),
+        addGpxMarker(map, "es-4655-103", 40.752000, -74.002000),
+        addGpxMarker(map, "es-4655-107", 40.752167, -74.001167),
+        addGpxMarker(map, "es-4655-114", 40.751000, -73.996333),
+        addGpxMarker(map, "es-4655-117", 40.751280, -73.995163),
+        addGpxMarker(map, "es-4655-123", 40.754887, -74.091103),
+        addGpxMarker(map, "es-4656-018", 39.387333, -76.348167),
+        addGpxMarker(map, "es-4656-033", 38.891000, -77.003500),
+    ]
+}
