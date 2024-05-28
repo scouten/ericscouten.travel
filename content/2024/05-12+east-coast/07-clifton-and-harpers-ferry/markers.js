af07559ff57bc949729177dbf3716eade2eca8dc
Eric Scouten <git@scouten.me>
2024-05-27 20:22:59 -0700

    East Coast Part 7: Clifton and Harpers Ferry

---
+++ content/2024/05-12+east-coast/07-clifton-and-harpers-ferry/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4657-007", 38.779167, -77.386000),
+        addGpxMarker(map, "es-4657-013", 38.780333, -77.387167),
+        addGpxMarker(map, "es-4657-014", 38.781000, -77.387333),
+        addGpxMarker(map, "es-4657-021", 39.322333, -77.740000),
+        addGpxMarker(map, "es-4657-024", 39.322195, -77.740128),
+        addGpxMarker(map, "es-4657-030", 39.322013, -77.740005),
+        addGpxMarker(map, "es-4657-033", 39.322017, -77.739700),
+    ]
+}
