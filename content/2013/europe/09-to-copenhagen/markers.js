6764eae14e776847a05b47954d52ac79d76600a4
Eric Scouten <git@scouten.me>
2024-06-27 20:20:18 -0700

    Europe Day 9: To Copenhagen

---
+++ content/2013/europe/09-to-copenhagen/markers.js
@@ -0,0 +1,16 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2373-006", 55.673333, 12.564667),
+        addGpxMarker(map, "es-2372-015", 55.679537, 12.591330),
+        addGpxMarker(map, "ts-2374-160", 55.673500, 12.565333),
+        addGpxMarker(map, "es-2372-017", 55.680415, 12.589658),
+        addGpxMarker(map, "es-2372-023", 55.680387, 12.589608),
+        addGpxMarker(map, "es-2372-025", 55.680383, 12.589597),
+        addGpxMarker(map, "es-2372-053", 55.693490, 12.598675),
+        addGpxMarker(map, "es-2372-058", 55.690263, 12.585837),
+        addGpxMarker(map, "es-2372-063", 55.687103, 12.577492),
+        addGpxMarker(map, "ts-2374-170", 55.682000, 12.574167),
+        addGpxMarker(map, "es-2372-065", 55.672705, 12.565300),
+        addGpxMarker(map, "es-2376-001", 55.607333, 13.001500),
+    ]
+}
