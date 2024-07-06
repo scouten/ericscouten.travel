615484a35313ed1da28de7940ecb2d1e6ba59385
Eric Scouten <git@scouten.me>
2024-07-05 21:06:51 -0700

    Europe Day 22: A Day Trip to Crivitz

---
+++ content/2013/europe/22-a-day-trip-to-crivitz/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2392-003", 53.566387, 10.532682),
+        addGpxMarker(map, "es-2393-001", 53.573882, 11.651362),
+        addGpxMarker(map, "es-2393-002", 53.573882, 11.651362),
+        addGpxMarker(map, "es-2393-003", 53.573882, 11.651362),
+        addGpxMarker(map, "es-2393-011", 53.577195, 11.648687),
+        addGpxMarker(map, "es-2393-014", 53.576727, 11.649028),
+        addGpxMarker(map, "es-2393-015", 53.576727, 11.649028),
+        addGpxMarker(map, "es-2393-019", 53.576430, 11.650063),
+        addGpxMarker(map, "es-2393-023", 53.577072, 11.650825),
+        addGpxMarker(map, "es-2394-009", 53.577788, 11.650313),
+        addGpxMarker(map, "es-2394-011", 53.578177, 11.649833),
+    ]
+}
