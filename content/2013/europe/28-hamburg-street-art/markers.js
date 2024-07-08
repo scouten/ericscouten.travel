54f28db2f8adf7c9b28b41ee15b63a4fb53d7e59
Eric Scouten <git@scouten.me>
2024-07-07 21:23:19 -0700

    Europe Days 23 – 28: Hamburg Street Art

---
+++ content/2013/europe/28-hamburg-street-art/markers.js
@@ -0,0 +1,9 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "ts-2404-012", 53.552167, 9.940167),
+        addGpxMarker(map, "es-2402-001", 53.547667, 9.954333),
+        addGpxMarker(map, "ts-2404-024", 53.551833, 9.941167),
+        addGpxMarker(map, "es-2405-014", 53.550500, 9.929667),
+        addGpxMarker(map, "es-2405-015", 53.550667, 9.929500),
+    ]
+}
