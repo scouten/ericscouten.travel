ce8b866a23664b050dad5ec7be9aa5da6c40b2d6
Eric Scouten <git@scouten.me>
2024-07-19 15:14:14 -0700

    Europe Days 24 – 26: Around Hamburg

---
+++ content/2013/europe/26-around-hamburg/markers.js
@@ -0,0 +1,8 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2400-002", 53.544137, 9.950308),
+        addGpxMarker(map, "es-2402-013", 53.569833, 9.735333),
+        addGpxMarker(map, "es-2403-003", 53.552313, 9.934388),
+        addGpxMarker(map, "es-2403-004", 53.552313, 9.934388),
+    ]
+}
--- content/2013/europe/26-welcome-to-hamburg/markers.js
+++
@@ -1,5 +0,0 @@
-function addGpxMarkers(map) {
-    return [
-        addGpxMarker(map, "es-2402-013", 53.569833, 9.735333),
-    ]
-}
