ae977cad5ed53c5a9a1d812a7230b08fbce1e3e3
Eric Scouten <git@scouten.me>
2024-07-20 18:59:10 -0700

    Add Adobe office photo to Fischmarkt

--- content/2013/europe/24-fischmarkt/markers.js
+++ content/2013/europe/24-fischmarkt/markers.js
@@ -5,5 +5,6 @@ function addGpxMarkers(map) {
         addGpxMarker(map, "es-2397-008", 53.545230, 9.952462),
         addGpxMarker(map, "es-2398-006a", 53.545033, 9.951462),
         addGpxMarker(map, "es-2398-009", 53.545033, 9.951462),
+        addGpxMarker(map, "es-2398-010", 53.544795, 9.950745),
     ]
 }
