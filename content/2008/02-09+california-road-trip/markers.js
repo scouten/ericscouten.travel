3d73577f693ad8c26699c1af4c9e88d549de9b63
Eric Scouten <git@scouten.me>
2024-08-03 22:17:50 -0700

    California Road Trip (2008)

---
+++ content/2008/02-09+california-road-trip/markers.js
@@ -0,0 +1,9 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-1267-038a", 41.811635, -124.105543),
+        addGpxMarker(map, "es-1267-083", 39.304865, -123.799960),
+        addGpxMarker(map, "es-1269-058", 39.007827, -121.866682),
+        addGpxMarker(map, "es-1271-081", 39.407530, -122.138725),
+        addGpxMarker(map, "es-1272-026", 39.407380, -122.154775),
+    ]
+}
