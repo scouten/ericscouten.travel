19af47d35ca856278f849ec022a2a5117f4cd58d
Eric Scouten <git@scouten.me>
2024-08-18 17:25:06 -0700

    Hello from Minnesota

---
+++ content/2009/04-17-hello-from-minnesota/markers.js
@@ -0,0 +1,6 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-1412-012", 44.647852, -93.699392),
+        addGpxMarker(map, "es-1412-081", 44.666265, -93.939633),
+    ]
+}
