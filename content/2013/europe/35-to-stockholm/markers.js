991dd325f27d18c9c58d51afba26cb2dc01a831f
Eric Scouten <git@scouten.me>
2024-08-26 12:14:22 -0700

    Europe Days 34 and 35: To Stockholm

---
+++ content/2013/europe/35-to-stockholm/markers.js
@@ -0,0 +1,10 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2419-002", 53.550500, 9.936000),
+        addGpxMarker(map, "es-2419-008", 54.653500, 11.351500),
+        addGpxMarker(map, "es-2423-002", 57.973547, 12.664192),
+        addGpxMarker(map, "es-2423-008", 57.978500, 12.658837),
+        addGpxMarker(map, "es-2423-016", 58.135370, 12.861703),
+        addGpxMarker(map, "es-2423-021", 58.134262, 12.860107),
+    ]
+}
