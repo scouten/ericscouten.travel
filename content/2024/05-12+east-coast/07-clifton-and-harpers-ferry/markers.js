b07d3bffd197f513e56a0d85e28ba4fd43f04454
Eric Scouten <git@scouten.me>
2024-08-25 18:27:20 -0700

    Add two of Lisa's photos from Harpers Ferry

--- content/2024/05-12+east-coast/07-clifton-and-harpers-ferry/markers.js
+++ content/2024/05-12+east-coast/07-clifton-and-harpers-ferry/markers.js
@@ -4,7 +4,9 @@ function addGpxMarkers(map) {
         addGpxMarker(map, "es-4657-013", 38.780333, -77.387167),
         addGpxMarker(map, "es-4657-014", 38.781000, -77.387333),
         addGpxMarker(map, "es-4657-021", 39.322333, -77.740000),
+        addGpxMarker(map, "lr-245-5198", 39.322187, -77.740372),
         addGpxMarker(map, "es-4657-024", 39.322195, -77.740128),
+        addGpxMarker(map, "lr-245-5200", 39.322097, -77.739937),
         addGpxMarker(map, "es-4657-030", 39.322013, -77.740005),
         addGpxMarker(map, "es-4657-033", 39.322017, -77.739700),
     ]
