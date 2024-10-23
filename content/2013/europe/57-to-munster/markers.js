3c287097927dd3a8e8a0022c570f9fdef962d53e
Eric Scouten <git@scouten.me>
2024-10-22 21:12:07 -0700

    Europe Day 57: To Münster

---
+++ content/2013/europe/57-to-munster/markers.js
@@ -0,0 +1,8 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2449-010", 51.963500, 7.626167),
+        addGpxMarker(map, "es-2452-002", 51.961828, 7.627877),
+        addGpxMarker(map, "es-2452-008", 51.961818, 7.627763),
+        addGpxMarker(map, "es-2452-011", 51.961818, 7.627763),
+    ]
+}
