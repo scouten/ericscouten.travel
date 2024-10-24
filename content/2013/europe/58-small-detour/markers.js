fa38ba3ee0dc7fab86fc50e1191d7256baa31d55
Eric Scouten <git@scouten.me>
2024-10-23 21:09:19 -0700

    Europe Day 58: A Small Detour

---
+++ content/2013/europe/58-small-detour/markers.js
@@ -0,0 +1,14 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2456-022", 52.367260, 4.898343),
+        addGpxMarker(map, "es-2456-023", 52.367172, 4.899075),
+        addGpxMarker(map, "es-2456-044", 52.365742, 4.900972),
+        addGpxMarker(map, "es-2456-050", 52.365612, 4.900402),
+        addGpxMarker(map, "es-2456-055", 52.364797, 4.895817),
+        addGpxMarker(map, "es-2456-070", 52.367990, 4.886700),
+        addGpxMarker(map, "es-2456-102", 52.375992, 4.889040),
+        addGpxMarker(map, "es-2456-160", 52.378458, 4.910110),
+        addGpxMarker(map, "es-2456-177", 52.371483, 4.904905),
+        addGpxMarker(map, "es-2454-010", 52.365688, 4.890397),
+    ]
+}
