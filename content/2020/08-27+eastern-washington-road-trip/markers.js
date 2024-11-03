c4bcf173747f8d1b7d1516bcbf3451b8d1c4c872
Eric Scouten <git@scouten.me>
2024-11-03 13:35:05 -0800

    Eastern Washington Road Trip (#2)

---
+++ content/2020/08-27+eastern-washington-road-trip/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4113-004", 47.480125, -118.254555),
+        addGpxMarker(map, "es-4113-005", 47.480828, -118.254647),
+        addGpxMarker(map, "es-4113-008", 47.907742, -118.289153),
+        addGpxMarker(map, "es-4113-010", 48.012458, -118.248528),
+        addGpxMarker(map, "es-4113-026", 48.691487, -118.083770),
+        addGpxMarker(map, "es-4113-032", 47.272270, -117.282562),
+        addGpxMarker(map, "es-4113-035", 47.264130, -117.269913),
+        addGpxMarker(map, "es-4114-005", 47.444600, -117.930345),
+        addGpxMarker(map, "es-4115-005", 47.350362, -117.231667),
+        addGpxMarker(map, "es-4115-019", 47.099772, -117.372130),
+        addGpxMarker(map, "es-4115-060", 47.261250, -118.691428),
+    ]
+}
