76f414c1f19ac41ace209292aa3164e73fb26282
Eric Scouten <git@scouten.me>
2024-11-04 20:25:44 -0800

    Europe Day 61: Prague!

---
+++ content/2013/europe/61-prague/markers.js
@@ -0,0 +1,14 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2458-066", 50.087765, 14.420732),
+        addGpxMarker(map, "es-2458-122", 50.087508, 14.420900),
+        addGpxMarker(map, "es-2458-138", 50.087280, 14.406998),
+        addGpxMarker(map, "es-2458-160", 50.086967, 14.408057),
+        addGpxMarker(map, "es-2458-169", 50.086917, 14.408323),
+        addGpxMarker(map, "es-2458-170", 50.086883, 14.408608),
+        addGpxMarker(map, "es-2458-173", 50.086697, 14.409552),
+        addGpxMarker(map, "es-2458-176", 50.086598, 14.410483),
+        addGpxMarker(map, "es-2458-183", 50.086565, 14.411072),
+        addGpxMarker(map, "es-2458-191", 50.086565, 14.411072),
+    ]
+}
