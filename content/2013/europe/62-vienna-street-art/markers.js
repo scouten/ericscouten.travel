db763391b281c6d104bf25fd91218b64ed39234b
Eric Scouten <git@scouten.me>
2024-11-09 08:32:23 -0800

    Europe Day 62: Vienna Street Art

---
+++ content/2013/europe/62-vienna-street-art/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2463-006", 48.212662, 16.376962),
+        addGpxMarker(map, "es-2463-013", 48.213157, 16.376428),
+        addGpxMarker(map, "es-2463-016", 48.213662, 16.375613),
+        addGpxMarker(map, "es-2463-017", 48.214238, 16.375235),
+        addGpxMarker(map, "es-2463-018", 48.214520, 16.375147),
+        addGpxMarker(map, "es-2463-020", 48.216277, 16.373747),
+        addGpxMarker(map, "es-2463-023", 48.216568, 16.373538),
+        addGpxMarker(map, "es-2463-033", 48.218443, 16.370828),
+        addGpxMarker(map, "es-2463-036", 48.220272, 16.368623),
+        addGpxMarker(map, "es-2463-043", 48.250730, 16.372320),
+        addGpxMarker(map, "es-2463-044", 48.254672, 16.371053),
+    ]
+}
