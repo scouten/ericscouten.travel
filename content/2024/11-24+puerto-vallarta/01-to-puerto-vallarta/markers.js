cc51bac086d762282869a91ff72af9a12f53b333
Eric Scouten <git@scouten.me>
2024-12-10 19:56:32 -0800

    Mexico Part 1: To Puerto Vallarta

---
+++ content/2024/11-24+puerto-vallarta/01-to-puerto-vallarta/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4706-037", 20.682500, -105.288167),
+        addGpxMarker(map, "lr-24b-5824", 20.683212, -105.288047),
+        addGpxMarker(map, "es-4706-079", 20.683167, -105.288000),
+        addGpxMarker(map, "lr-24b-5920", 20.682675, -105.284988),
+        addGpxMarker(map, "es-4706-230", 20.682667, -105.284833),
+        addGpxMarker(map, "es-4706-233", 20.682700, -105.284800),
+        addGpxMarker(map, "es-4706-240", 20.682833, -105.284667),
+    ]
+}
