234d56b8738cf9c2b551287f31d1b065cdcbca41
Eric Scouten <git@scouten.me>
2024-12-15 20:27:54 -0800

    Mexico Part 2: Camino Viejo

---
+++ content/2024/11-24+puerto-vallarta/02-camino-viejo/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "lr-24b-5814", 20.833025, -105.449408),
+        addGpxMarker(map, "lr-24b-5816a", 20.864803, -105.444953),
+        addGpxMarker(map, "es-4706-045", 20.869333, -105.438667),
+        addGpxMarker(map, "es-4706-050", 20.871500, -105.439833),
+        addGpxMarker(map, "lr-24b-5819", 20.871570, -105.439920),
+        addGpxMarker(map, "es-4706-052", 20.871833, -105.440000),
+        addGpxMarker(map, "lr-24b-5821", 20.872030, -105.439963),
+        addGpxMarker(map, "es-4706-056a", 21.084667, -105.167167),
+        addGpxMarker(map, "es-4706-059", 21.078833, -105.160833),
+        addGpxMarker(map, "es-4706-061", 21.021167, -105.136000),
+        addGpxMarker(map, "es-4706-066", 20.988333, -105.127500),
+    ]
+}
