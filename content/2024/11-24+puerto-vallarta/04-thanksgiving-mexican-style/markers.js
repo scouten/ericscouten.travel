56b4b18ec88b69ed6d4b76983500975258e8b458
Eric Scouten <git@scouten.me>
2024-12-22 21:39:24 -0800

    Mexico Part 4: Thanksgiving, Mexican Style

---
+++ content/2024/11-24+puerto-vallarta/04-thanksgiving-mexican-style/markers.js
@@ -0,0 +1,14 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "lr-24b-5911", 20.602305, -105.238363),
+        addGpxMarker(map, "lr-24b-5912", 20.602283, -105.238403),
+        addGpxMarker(map, "es-4706-187", 20.603000, -105.238167),
+        addGpxMarker(map, "es-4706-199", 20.754333, -105.337333),
+        addGpxMarker(map, "es-4706-201", 20.753833, -105.336500),
+        addGpxMarker(map, "es-4706-205", 20.753167, -105.336833),
+        addGpxMarker(map, "es-4706-208", 20.753167, -105.337000),
+        addGpxMarker(map, "es-4706-210", 20.753000, -105.337167),
+        addGpxMarker(map, "es-4706-221", 20.754000, -105.337500),
+        addGpxMarker(map, "lr-24b-5914", 20.682775, -105.284795),
+    ]
+}
