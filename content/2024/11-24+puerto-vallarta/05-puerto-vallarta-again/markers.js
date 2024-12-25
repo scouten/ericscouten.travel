613c60082a4644cc74a3f10e4a06bf19588a8755
Eric Scouten <git@scouten.me>
2024-12-25 11:18:34 -0800

    Fix GPX markers for Mexico Part 5

--- content/2024/11-24+puerto-vallarta/05-puerto-vallarta-again/markers.js
+++ content/2024/11-24+puerto-vallarta/05-puerto-vallarta-again/markers.js
@@ -1,14 +1,14 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4706-251", 20.611000, -105.234667),
-        addGpxMarker(map, "lr-24b-5934", 20.608525, -105.235297),
-        addGpxMarker(map, "es-4706-266", 20.610667, -105.234500),
-        addGpxMarker(map, "es-4706-277", 20.615833, -105.230500),
-        addGpxMarker(map, "es-4706-285", 20.615333, -105.230667),
-        addGpxMarker(map, "es-4706-290", 20.615667, -105.230500),
-        addGpxMarker(map, "es-4706-291", 20.615667, -105.230500),
-        addGpxMarker(map, "es-4706-295", 20.665833, -105.250833),
-        addGpxMarker(map, "es-4706-296", 20.664833, -105.251500),
-        addGpxMarker(map, "es-4706-304", 20.693167, -105.243167),
+        addCdnGpxMarker(map, "es-4706-251", 20.611000, -105.234667, "img/v1/2024/11/es-4706-251"),
+        addCdnGpxMarker(map, "lr-24b-5934", 20.608525, -105.235297, "img/v1/2024/11/lr-24b-5934"),
+        addCdnGpxMarker(map, "es-4706-266", 20.610667, -105.234500, "img/v1/2024/11/es-4706-266"),
+        addCdnGpxMarker(map, "es-4706-277", 20.615833, -105.230500, "img/v1/2024/11/es-4706-277"),
+        addCdnGpxMarker(map, "es-4706-285", 20.615333, -105.230667, "img/v1/2024/11/es-4706-285"),
+        addCdnGpxMarker(map, "es-4706-290", 20.615667, -105.230500, "img/v1/2024/11/es-4706-290"),
+        addCdnGpxMarker(map, "es-4706-291", 20.615667, -105.230500, "img/v1/2024/11/es-4706-291"),
+        addCdnGpxMarker(map, "es-4706-295", 20.665833, -105.250833, "img/v1/2024/11/es-4706-295"),
+        addCdnGpxMarker(map, "es-4706-296", 20.664833, -105.251500, "img/v1/2024/11/es-4706-296"),
+        addCdnGpxMarker(map, "es-4706-304", 20.693167, -105.243167, "img/v1/2024/11/es-4706-304"),
     ]
 }
