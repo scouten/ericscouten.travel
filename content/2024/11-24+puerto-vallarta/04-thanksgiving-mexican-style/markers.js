2fb3aa41252515ab1753be3ff4c2a9cc285bf417
Eric Scouten <git@scouten.me>
2024-12-25 13:11:23 -0800

    CDN conversion for Mex part 4

--- content/2024/11-24+puerto-vallarta/04-thanksgiving-mexican-style/markers.js
+++ content/2024/11-24+puerto-vallarta/04-thanksgiving-mexican-style/markers.js
@@ -1,14 +1,14 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "lr-24b-5911", 20.602305, -105.238363),
-        addGpxMarker(map, "lr-24b-5912", 20.602283, -105.238403),
-        addGpxMarker(map, "es-4706-187", 20.603000, -105.238167),
-        addGpxMarker(map, "es-4706-199", 20.754333, -105.337333),
-        addGpxMarker(map, "es-4706-201", 20.753833, -105.336500),
-        addGpxMarker(map, "es-4706-205", 20.753167, -105.336833),
-        addGpxMarker(map, "es-4706-208", 20.753167, -105.337000),
-        addGpxMarker(map, "es-4706-210", 20.753000, -105.337167),
-        addGpxMarker(map, "es-4706-221", 20.754000, -105.337500),
-        addGpxMarker(map, "lr-24b-5914", 20.682775, -105.284795),
+        addCdnGpxMarker(map, "lr-24b-5911", 20.602305, -105.238363, "img/v1/2024/11/lr-24b-5911"),
+        addCdnGpxMarker(map, "lr-24b-5912", 20.602283, -105.238403, "img/v1/2024/11/lr-24b-5912"),
+        addCdnGpxMarker(map, "es-4706-187", 20.603000, -105.238167, "img/v1/2024/11/es-4706-187"),
+        addCdnGpxMarker(map, "es-4706-199", 20.754333, -105.337333, "img/v1/2024/11/es-4706-199"),
+        addCdnGpxMarker(map, "es-4706-201", 20.753833, -105.336500, "img/v1/2024/11/es-4706-201"),
+        addCdnGpxMarker(map, "es-4706-205", 20.753167, -105.336833, "img/v1/2024/11/es-4706-205"),
+        addCdnGpxMarker(map, "es-4706-208", 20.753167, -105.337000, "img/v1/2024/11/es-4706-208"),
+        addCdnGpxMarker(map, "es-4706-210", 20.753000, -105.337167, "img/v1/2024/11/es-4706-210"),
+        addCdnGpxMarker(map, "es-4706-221", 20.754000, -105.337500, "img/v1/2024/11/es-4706-221"),
+        addCdnGpxMarker(map, "lr-24b-5914", 20.682775, -105.284795, "img/v1/2024/11/lr-24b-5914"),
     ]
 }
