d3f096c74c2b55ea7fa3ebdf6008eb43327c6aa3
Eric Scouten <git@scouten.me>
2024-12-26 11:43:18 -0800

    CDN retrofit for Washington Pass

--- content/2024/06-29-central-washington-and-washington-pass/markers.js
+++ content/2024/06-29-central-washington-and-washington-pass/markers.js
@@ -1,12 +1,12 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4675-021p", 48.525833, -120.647167),
-        addGpxMarker(map, "es-4675-036", 48.525833, -120.647333),
-        addGpxMarker(map, "es-4675-042", 48.525833, -120.647167),
-        addGpxMarker(map, "es-4675-048", 48.526333, -120.647167),
-        addGpxMarker(map, "lr-246-5311", 48.526355, -120.647147),
-        addGpxMarker(map, "lr-246-5312", 48.526378, -120.647255),
-        addGpxMarker(map, "es-4675-051", 48.526500, -120.647333),
+        addCdnGpxMarker(map, "es-4675-021p", 48.525833, -120.647167, "img/v1/2024/06/es-4675-021p"),
+        addCdnGpxMarker(map, "es-4675-036", 48.525833, -120.647333, "img/v1/2024/06/es-4675-036"),
+        addCdnGpxMarker(map, "es-4675-042", 48.525833, -120.647167, "img/v1/2024/06/es-4675-042"),
+        addCdnGpxMarker(map, "es-4675-048", 48.526333, -120.647167, "img/v1/2024/06/es-4675-048"),
+        addCdnGpxMarker(map, "lr-246-5311", 48.526355, -120.647147, "img/v1/2024/06/lr-246-5311"),
+        addCdnGpxMarker(map, "lr-246-5312", 48.526378, -120.647255, "img/v1/2024/06/lr-246-5312"),
+        addCdnGpxMarker(map, "es-4675-051", 48.526500, -120.647333, "img/v1/2024/06/es-4675-051"),
 
         addParkMarker(map, 130, "Curlew Lake", "curlew-lake", 48.719, -118.664, "es-4676-052"),
         addParkMarker(map, 131, "Ranald MacDonald", "ranald-macdonald", 48.941, -118.759, "es-4676-096"),
