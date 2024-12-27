d096cb99587de3279f6f9071c475c0dad5d87853
Eric Scouten <git@scouten.me>
2024-12-26 19:13:37 -0800

    California Part 1: Window Seat

---
+++ content/2024/12-20+california/01-window-seat/markers.js
@@ -0,0 +1,8 @@
+function addGpxMarkers(map) {
+    return [
+        addCdnGpxMarker(map, "es-4710-003", 46.866698, -122.090443, "img/v1/2024/12/es-4710-003"),
+        addCdnGpxMarker(map, "es-4710-004", 34.841323, -118.385742, "img/v1/2024/12/es-4710-004"),
+        addCdnGpxMarker(map, "es-4710-007", 34.195765, -117.120085, "img/v1/2024/12/es-4710-007"),
+        addCdnGpxMarker(map, "es-4710-008", 33.775772, -116.253510, "img/v1/2024/12/es-4710-008"),
+    ]
+}
---
+++ content/2024/12-20+california/_index.md
@@ -0,0 +1,10 @@
++++
+title = "California"
+sort_by = "date"
+transparent = true
+
+[extra]
+# cover_abs = "/2024/02-07+cabo/01-to-cabo/cover-es-4632-133.jpg"
++++
+
+I'm taking a few days to myself to enjoy the southern California desert.
