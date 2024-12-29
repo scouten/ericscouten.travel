b7f074573976552c6a2e114303f3a3d72e133098
Eric Scouten <git@scouten.me>
2024-12-28 21:23:07 -0800

    CDN retrofit for Hood River

--- content/2024/03-23-long-way-to-hood-river/markers.js
+++ content/2024/03-23-long-way-to-hood-river/markers.js
@@ -1,11 +1,11 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4638-001", 44.877833, -121.429167),
-        addGpxMarker(map, "es-4638-011", 44.872667, -121.442500),
-        addGpxMarker(map, "es-4638-019", 44.785000, -121.317667),
-        addGpxMarker(map, "es-4638-022", 44.785167, -121.317833),
-        addGpxMarker(map, "es-4638-037", 45.021833, -120.991500),
-        addGpxMarker(map, "es-4638-061", 45.165000, -121.087333),
-        addGpxMarker(map, "es-4639-005", 45.709032, -121.511463),
+        addCdnGpxMarker(map, "es-4638-001d", 44.877833, -121.429167, "img/v1/2024/03/es-4638-001d"),
+        addCdnGpxMarker(map, "es-4638-011d", 44.872667, -121.442500, "img/v1/2024/03/es-4638-011d"),
+        addCdnGpxMarker(map, "es-4638-019d", 44.785000, -121.317667, "img/v1/2024/03/es-4638-019d"),
+        addCdnGpxMarker(map, "es-4638-022d", 44.785167, -121.317833, "img/v1/2024/03/es-4638-022d"),
+        addCdnGpxMarker(map, "es-4638-037d", 45.021833, -120.991500, "img/v1/2024/03/es-4638-037d"),
+        addCdnGpxMarker(map, "es-4638-061d", 45.165000, -121.087333, "img/v1/2024/03/es-4638-061d"),
+        addCdnGpxMarker(map, "es-4639-005", 45.709032, -121.511463, "img/v1/2024/03/es-4639-005"),
     ]
 }
