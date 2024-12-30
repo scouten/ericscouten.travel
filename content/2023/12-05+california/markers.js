ec0921edbb061856c153d5b96988f1cec08849e1
Eric Scouten <git@scouten.me>
2024-12-30 10:06:04 -0800

    CDN retrofit for Dec 2023 California trip

--- content/2023/12-05+california/markers.js
+++ content/2023/12-05+california/markers.js
@@ -1,10 +1,10 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4617-030", 47.463667, -122.306333),
-        addGpxMarker(map, "es-4618-014", 37.331167, -121.892833),
-        addGpxMarker(map, "es-4620-026", 37.333667, -121.890833),
-        addGpxMarker(map, "es-4620-043", 37.913760, -121.987338),
-        addGpxMarker(map, "es-4620-046", 41.332347, -121.934763),
-        addGpxMarker(map, "es-4620-051", 47.631458, -122.405278),
+        addCdnGpxMarker(map, "es-4617-030", 47.463667, -122.306333, "img/v1/2023/12/es-4617-030"),
+        addCdnGpxMarker(map, "es-4618-014", 37.331167, -121.892833, "img/v1/2023/12/es-4618-014"),
+        addCdnGpxMarker(map, "es-4620-026", 37.333667, -121.890833, "img/v1/2023/12/es-4620-026"),
+        addCdnGpxMarker(map, "es-4620-043", 37.913760, -121.987338, "img/v1/2023/12/es-4620-043"),
+        addCdnGpxMarker(map, "es-4620-046", 41.332347, -121.934763, "img/v1/2023/12/es-4620-046"),
+        addCdnGpxMarker(map, "es-4620-051", 47.631458, -122.405278, "img/v1/2023/12/es-4620-051"),
     ]
 }
