996902b0d0b45d80404031857092437863f3d342
Eric Scouten <git@scouten.me>
2024-12-29 19:22:12 -0800

    California Day 3: Joshua Tree and Points South

---
+++ content/2024/12-20+california/03-joshua-tree-and-points-south/markers.js
@@ -0,0 +1,22 @@
+function addGpxMarkers(map) {
+    return [
+        addCdnGpxMarker(map, "es-4711-008", 33.980500, -116.013833, "img/v1/2024/12/es-4711-008"),
+        addCdnGpxMarker(map, "es-4711-011", 33.980833, -116.014000, "img/v1/2024/12/es-4711-011"),
+        addCdnGpxMarker(map, "es-4711-013", 33.980833, -116.014167, "img/v1/2024/12/es-4711-013"),
+        addCdnGpxMarker(map, "es-4711-020", 33.980333, -116.014000, "img/v1/2024/12/es-4711-020"),
+        addCdnGpxMarker(map, "es-4711-025", 33.925833, -115.931833, "img/v1/2024/12/es-4711-025"),
+        addCdnGpxMarker(map, "es-4711-035", 33.925500, -115.932000, "img/v1/2024/12/es-4711-035"),
+        addCdnGpxMarker(map, "es-4711-036", 33.925500, -115.932000, "img/v1/2024/12/es-4711-036"),
+        addCdnGpxMarker(map, "es-4711-049", 33.908753, -115.857567, "img/v1/2024/12/es-4711-049"),
+        addCdnGpxMarker(map, "es-4711-053", 33.788167, -115.792833, "img/v1/2024/12/es-4711-053"),
+        addCdnGpxMarker(map, "es-4711-062", 33.736667, -115.810333, "img/v1/2024/12/es-4711-062"),
+        addCdnGpxMarker(map, "es-4711-072", 33.736167, -115.809500, "img/v1/2024/12/es-4711-072"),
+        addCdnGpxMarker(map, "es-4711-084", 33.675500, -115.801500, "img/v1/2024/12/es-4711-084"),
+        addCdnGpxMarker(map, "es-4711-090", 33.604333, -115.921667, "img/v1/2024/12/es-4711-090"),
+        addCdnGpxMarker(map, "es-4711-105", 33.589333, -115.961500, "img/v1/2024/12/es-4711-105"),
+        addCdnGpxMarker(map, "es-4711-106", 33.589333, -115.961333, "img/v1/2024/12/es-4711-106"),
+        addCdnGpxMarker(map, "es-4711-110", 33.589333, -115.961500, "img/v1/2024/12/es-4711-110"),
+        addCdnGpxMarker(map, "es-4711-115", 33.459167, -115.863333, "img/v1/2024/12/es-4711-115"),
+        addCdnGpxMarker(map, "es-4711-120", 33.459333, -115.863167, "img/v1/2024/12/es-4711-120"),
+    ]
+}
