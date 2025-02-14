a0ee4f05e00aeef7605cbb9c4a5e006729a69eeb
Eric Scouten <git@scouten.me>
2025-02-14 13:42:49 -0800

    CDN retrofit for Long Island '02

--- content/2002/02-23+long-island/markers.js
+++ content/2002/02-23+long-island/markers.js
@@ -1,15 +1,15 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-0508-026", 41.103781, -72.359572),
-        addGpxMarker(map, "es-0509-006", 41.133019, -72.325931),
-        addGpxMarker(map, "es-0509-033", 41.072531, -71.857450),
-        addGpxMarker(map, "es-0510-003", 40.833831, -72.506119),
-        addGpxMarker(map, "es-0510-022", 40.833831, -72.506119),
-        addGpxMarker(map, "es-0510-027", 40.833831, -72.506119),
-        addGpxMarker(map, "es-0511-024", 40.833831, -72.506119),
-        addGpxMarker(map, "es-0512-012", 40.833831, -72.506119),
-        addGpxMarker(map, "es-0512-022", 40.833831, -72.506119),
-        addGpxMarker(map, "es-0513-013", 40.632361, -73.217861),
-        addGpxMarker(map, "es-0513-019", 40.634346, -73.216005),
+        addCdnGpxMarker(map, "es-0508-026", 41.103781, -72.359572, "img/v1/2002/02/es-0508-026"),
+        addCdnGpxMarker(map, "es-0509-006", 41.133019, -72.325931, "img/v1/2002/02/es-0509-006"),
+        addCdnGpxMarker(map, "es-0509-033", 41.072531, -71.857450, "img/v1/2002/02/es-0509-033"),
+        addCdnGpxMarker(map, "es-0510-003", 40.833831, -72.506119, "img/v1/2002/02/es-0510-003"),
+        addCdnGpxMarker(map, "es-0510-022", 40.833831, -72.506119, "img/v1/2002/02/es-0510-022"),
+        addCdnGpxMarker(map, "es-0510-027", 40.833831, -72.506119, "img/v1/2002/02/es-0510-027"),
+        addCdnGpxMarker(map, "es-0511-024", 40.833831, -72.506119, "img/v1/2002/02/es-0511-024"),
+        addCdnGpxMarker(map, "es-0512-012", 40.833831, -72.506119, "img/v1/2002/02/es-0512-012"),
+        addCdnGpxMarker(map, "es-0512-022", 40.833831, -72.506119, "img/v1/2002/02/es-0512-022"),
+        addCdnGpxMarker(map, "es-0513-013", 40.632361, -73.217861, "img/v1/2002/02/es-0513-013"),
+        addCdnGpxMarker(map, "es-0513-019", 40.634346, -73.216005, "img/v1/2002/02/es-0513-019"),
     ]
 }
