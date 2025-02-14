f538d8420369697049de579db3798ed031d7499b
Eric Scouten <git@scouten.me>
2025-02-14 07:09:23 -0800

    CDN retrofit for A Farm and Some Cliffs

--- content/2017/06-24+europe/18-a-farm-and-some-cliffs/markers.js
+++ content/2017/06-24+europe/18-a-farm-and-some-cliffs/markers.js
@@ -1,15 +1,15 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-3532-001", 52.751389, -8.917222),
-        addGpxMarker(map, "es-3532-004", 52.751389, -8.917222),
-        addGpxMarker(map, "ts-3534-001", 52.938889, -9.293056),
-        addGpxMarker(map, "es-3532-009", 52.970406, -9.427926),
-        addGpxMarker(map, "es-3532-011", 52.970219, -9.427971),
-        addGpxMarker(map, "es-3532-012", 52.970075, -9.428004),
-        addGpxMarker(map, "es-3532-026", 52.968369, -9.431024),
-        addGpxMarker(map, "es-3532-028", 52.968065, -9.431060),
-        addGpxMarker(map, "es-3532-029", 52.968471, -9.430931),
-        addGpxMarker(map, "es-3532-031", 52.968848, -9.429264),
-        addGpxMarker(map, "es-3533-005", 52.947906, -9.353939),
+        addCdnGpxMarker(map, "es-3532-001", 52.751389, -8.917222, "img/v1/2017/07/es-3532-001"),
+        addCdnGpxMarker(map, "es-3532-004", 52.751389, -8.917222, "img/v1/2017/07/es-3532-004"),
+        addCdnGpxMarker(map, "ts-3534-001", 52.938889, -9.293056, "img/v1/2017/07/ts-3534-001"),
+        addCdnGpxMarker(map, "es-3532-009", 52.970406, -9.427926, "img/v1/2017/07/es-3532-009"),
+        addCdnGpxMarker(map, "es-3532-011", 52.970219, -9.427971, "img/v1/2017/07/es-3532-011"),
+        addCdnGpxMarker(map, "es-3532-012", 52.970075, -9.428004, "img/v1/2017/07/es-3532-012"),
+        addCdnGpxMarker(map, "es-3532-026", 52.968369, -9.431024, "img/v1/2017/07/es-3532-026"),
+        addCdnGpxMarker(map, "es-3532-028", 52.968065, -9.431060, "img/v1/2017/07/es-3532-028"),
+        addCdnGpxMarker(map, "es-3532-029", 52.968471, -9.430931, "img/v1/2017/07/es-3532-029"),
+        addCdnGpxMarker(map, "es-3532-031", 52.968848, -9.429264, "img/v1/2017/07/es-3532-031"),
+        addCdnGpxMarker(map, "es-3533-005", 52.947906, -9.353939, "img/v1/2017/07/es-3533-005"),
     ]
 }
